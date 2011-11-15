a8c6864909b9b6b8b382b51997b98200ab922112
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-11-15 16:33:08 -0500

    add arrays.clone, improve arrays.copy
    These methods work with typed arrays now.
    
    Also added an arrays.constructor(source) function.
    
    WIP

--- examples/simplemolecules/arrays.js
+++ examples/simplemolecules/arrays.js
@@ -20,136 +20,180 @@
   // Int32Array
   // Float32Array
   
-  arrays.create - function(size, fill, array_type) {
-      fill = fill || 0;
-      var a, i;
-      if (array_type === "regular") {
-          a = new Array(size);
+  arrays.create = function(size, fill, array_type) {
+    if (!array_type) {
+      if (arrays.webgl) {
+        array_type = "Float32Array"
       } else {
-        switch(array_type) {
-        case "Float64Array":
-          a = new Float64Array(size);
-          break;
-        case "Float32Array":
-          a = new Float32Array(size);
-          break;
-        case "Int32Array":
-          a = new Int32Array(size);
-          break;
-        case "Int16Array":
-          a = new Int16Array(size);
-          break;
-        case "Int8Array":
-          a = new Int8Array(size);
-          break;
-        case "Uint32Array":
-          a = new Uint32Array(size);
-          break;
-        case "Uint16Array":
-          a = new Uint16Array(size);
-          break;
-        case "Uint8Array":
-          a = new Uint8Array(size);
-          break;
-        default:
-          a = new Array(size);
-          break;
-        }
+        array_type = "regular"
       }
-      i=-1; while(++i < size) { a[i] = fill };
-      return a;
+    }
+    fill = fill || 0;
+    var a, i;
+    if (array_type === "regular") {
+      a = new Array(size);
+    } else {
+      switch(array_type) {
+        case "Float64Array":
+        a = new Float64Array(size);
+        break;
+        case "Float32Array":
+        a = new Float32Array(size);
+        break;
+        case "Int32Array":
+        a = new Int32Array(size);
+        break;
+        case "Int16Array":
+        a = new Int16Array(size);
+        break;
+        case "Int8Array":
+        a = new Int8Array(size);
+        break;
+        case "Uint32Array":
+        a = new Uint32Array(size);
+        break;
+        case "Uint16Array":
+        a = new Uint16Array(size);
+        break;
+        case "Uint8Array":
+        a = new Uint8Array(size);
+        break;
+        default:
+        a = new Array(size);
+        break;
+      }
+    }
+    i=-1; while(++i < size) { a[i] = fill };
+    return a;
   }
 
-  arrays.copy = function(destination, source) {
-      var i, source_length = source.length;
+  arrays.constructor_function = function(source) {
+    if (source.buffer && source.buffer.__proto__.constructor) {
+      return source.__proto__.constructor
+    }
+    if (source.constructor === Array) {
+      return source.constructor
+    }
+    throw new Error(
+        "arrays.constructor_function: must be an Array or Typed Array: " +
+        "  source: " + source +
+        ", source.constructor: " + source.constructor +
+        ", source.buffer: " + source.buffer +
+        ", source.buffer.slice: " + source.buffer.slice +
+        ", source.buffer.__proto__: " + source.buffer.__proto__ +
+        ", source.buffer.__proto__.constructor: " + source.buffer.__proto__.constructor
+      )
+  }
+
+  arrays.copy = function(source, dest) {
+    var len = source.length; i = -1; 
+    while(++i < len) { dest[i] = source[i] }
+    dest.length = len;
+    return dest
+  }
+
+  arrays.clone = function(source) {
+    var i, len = source.length, clone, constructor;
+    constructor = arrays.constructor_function(source);
+    if (constructor == Array) {
+      clone = new constructor(len);
       for (i = 0; i < source_length; i++) { destination[i] = source[i]; }
+      return clone
+    }
+    if (source.buffer.slice) {
+      clone = new constructor(source.buffer.slice(0));
+      return clone
+    }
+    clone = new constructor(len);
+    for (i = 0; i < len; i++) { clone[i] = source[i]; }
+    return clone
   }
 
   /** @return true if x is between a and b. */
   // float a, float b, float x
-   arrays.between = function(a, b, x) {
-      return x < Math.max(a, b) && x > Math.min(a, b);
+  arrays.between = function(a, b, x) {
+    return x < Math.max(a, b) && x > Math.min(a, b);
   }
 
   // float[] array
   arrays.max = function(array) {
-      return Math.max.apply( Math, array );
+    return Math.max.apply( Math, array );
   }
 
   // float[] array
   arrays.min = function(array) {
-      return Math.min.apply( Math, array );
+    return Math.min.apply( Math, array );
   }
 
   // FloatxxArray[] array
   arrays.maxTypedArray = function(array) {
-      var test, i,
-          max = Number.MIN_VALUE,
-          length = array.length;
-      for(i = 0; i < length; i++) {
-          test = array[i];
-          max = test > max ? test : max;
-      }
-      return max;
+    var test, i,
+    max = Number.MIN_VALUE,
+    length = array.length;
+    for(i = 0; i < length; i++) {
+      test = array[i];
+      max = test > max ? test : max;
+    }
+    return max;
   }
 
   // FloatxxArray[] array
   arrays.minTypedArray = function(array) {
-      var test, i,
-          min = Number.MAX_VALUE,
-          length = array.length;
-      for(i = 0; i < length; i++) {
-          test = array[i];
-          min = test < min ? test : min;
-      }
-      return min;
+    var test, i,
+    min = Number.MAX_VALUE,
+    length = array.length;
+    for(i = 0; i < length; i++) {
+      test = array[i];
+      min = test < min ? test : min;
+    }
+    return min;
   }
 
   // float[] array
   arrays.maxAnyArray = function(array) {
-      try {
-          return Math.max.apply( Math, array );
+    try {
+      return Math.max.apply( Math, array );
+    }
+    catch (e) {
+      if (e instanceof TypeError) {
+        var test, i,
+        max = Number.MIN_VALUE,
+        length = array.length;
+        for(i = 0; i < length; i++) {
+          test = array[i];
+          max = test > max ? test : max;
+        }
+        return max;
       }
-      catch (e) {
-          if (e instanceof TypeError) {
-              var test, i,
-                  max = Number.MIN_VALUE,
-                  length = array.length;
-              for(i = 0; i < length; i++) {
-                  test = array[i];
-                  max = test > max ? test : max;
-              }
-              return max;
-          }
-      }
+    }
   }
 
   // float[] array
   arrays.minAnyArray = function(array) {
-      try {
-          return Math.min.apply( Math, array );
+    try {
+      return Math.min.apply( Math, array );
+    }
+    catch (e) {
+      if (e instanceof TypeError) {
+        var test, i,
+        min = Number.MAX_VALUE,
+        length = array.length;
+        for(i = 0; i < length; i++) {
+          test = array[i];
+          min = test < min ? test : min;
+        }
+        return min;
       }
-      catch (e) {
-          if (e instanceof TypeError) {
-              var test, i,
-                 min = Number.MAX_VALUE,
-                 length = array.length;
-              for(i = 0; i < length; i++) {
-                  test = array[i];
-                  min = test < min ? test : min;
-              }
-              return min;
-          }
-      }
+    }
   }
 
   arrays.average = function(array) {
-      var i, acc = 0,
-          length = array.length;
-      for (i = 0; i < length; i++) {
-          acc += array[i];
-      }
-      return acc / length;
+    var i, acc = 0,
+    length = array.length;
+    for (i = 0; i < length; i++) {
+      acc += array[i];
+    }
+    return acc / length;
   }
 
   // export namespace
