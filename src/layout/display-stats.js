183004bed28789f06e5aeac14dcc262e5c47a1cc
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-01-13 00:19:09 -0500

    don't display stats if the stats element isn't there

--- src/layout/display-stats.js
+++ src/layout/display-stats.js
@@ -11,12 +11,14 @@ layout.displayStats = function() {
       pe = model.pe(),
       te = ke + pe;
 
-  stats.textContent =
-    "Time: "     + d3.format("5.2f")(model.stepCounter() * sample_time) + " (ns), " +
-    "KE: "       + d3.format("1.4f")(ke) + ", " +
-    // "PE: "       + d3.format("1.6f")(pe) + ", " +
-    "TE: "       + d3.format("1.4f")(te) + ", " +
-    "Pressure: " + d3.format("6.3f")(model.pressure()) + ", " +
-    "Rate: " + d3.format("5.1f")(model.get_rate()) + " (steps/s)";
+  if (stats) {
+    stats.textContent =
+      "Time: "     + d3.format("5.2f")(model.stepCounter() * sample_time) + " (ns), " +
+      "KE: "       + d3.format("1.4f")(ke) + ", " +
+      // "PE: "       + d3.format("1.6f")(pe) + ", " +
+      "TE: "       + d3.format("1.4f")(te) + ", " +
+      "Pressure: " + d3.format("6.3f")(model.pressure()) + ", " +
+      "Rate: " + d3.format("5.1f")(model.get_rate()) + " (steps/s)";
+  }
 }
 
