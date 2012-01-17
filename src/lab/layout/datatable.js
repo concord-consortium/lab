1af5939af50af66d9864dd536d0ce86d65222fd5
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-01-17 14:56:06 -0500

    new module method: layout.hide_datatabl()
    partial isolation of access to datatable_table 
    element variable

--- src/lab/layout/datatable.js
+++ src/lab/layout/datatable.js
@@ -9,9 +9,14 @@ layout.datatable_visible = false;
 var toggle_datatable = document.getElementById("toggle-datatable");
 var datatable_table = document.getElementById("datatable-table");
 
+layout.hide_datatable = function() {
+  if (datatable_table) {
+    datatable_table.style.display = "none";
+  }
+}
+
 layout.render_datatable = function(reset) {
-  datatable_table = document.getElementById("datatable-table");
-  datatable_table.style.display = "none";
+  datatable_table.style.display = "";
   var i,
       titlerows = datatable_table.getElementsByClassName("title"),
       datarows = datatable_table.getElementsByClassName("data"),
