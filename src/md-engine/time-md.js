1935542e6bdd30d604428fcdb2a8f64b777dc0ac
Richard Klancer <rpk@pobox.com>
2012-03-15 08:40:16 -0400

    time-md command prints ms per integration

---
+++ src/md-engine/time-md.js
@@ -0,0 +1,47 @@
+/*jslint node:true */
+
+var model = require('./2d-molecules').model,
+    nodes,
+    radius, px, py, x, y, vx, vy, speed, ax, ay, halfmass, charge,
+    integrator, state;
+
+// obvious API fix: there should be no need to interrupt the 'var' statement to run createNodes()
+model.createNodes();
+
+nodes    = model.nodes;
+radius   = model.radius;
+px       = model.px;
+py       = model.py;
+x        = model.x;
+y        = model.y;
+vx       = model.vx;
+vy       = model.vy;
+speed    = model.speed;
+ax       = model.ax;
+ay       = model.ay;
+halfmass = model.halfmass;
+charge   = model.charge;
+
+integrator = model.getIntegrator();
+state      = integrator.getOutputState();
+
+printCM = function(cm) {
+  console.log(cm[0] + ', ' + cm[1]);
+};
+
+exports.run = function() {
+  var n = 500,
+      i, begin;
+
+  // warm up
+  for (i = 0; i < n; i++) {
+    integrator.integrate();
+  }
+
+  begin = Date.now();
+
+  for (i = 0; i < n; i++) {
+    integrator.integrate();
+  }
+  console.log( (Date.now() - begin) / n);
+};
