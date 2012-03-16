e5991d3159c469ec63e71b6f56809fd767b488e1
Richard Klancer <rpk@pobox.com>
2012-03-16 02:51:27 -0400

    Emit data for plotting/analyzing drift in CM

--- src/md-engine/run-md.js
+++ src/md-engine/run-md.js
@@ -1,9 +1,9 @@
 var model  = require('./md2d').model,
     format = require('d3').format,
     nodes,
-    radius, px, py, x, y, vx, vy, speed, ax, ay, halfmass, charge,
     integrator, state,
-    printCM;
+    dt = 0.05,
+    drift, initialCM, printDrift;
 
 // obvious API fix: there should be no need to interrupt the 'var' statement to run createNodes()
 model.createNodes();
@@ -13,22 +13,34 @@ nodes    = model.nodes;
 integrator = model.getIntegrator();
 state      = integrator.getOutputState();
 
-printCM = function() {
-  console.log(format('.2f')(state.time) + ', ' + format('.3f')(state.CM[0]) + ', ' + format('.3f')(state.CM[1]));
+initialCM = [state.CM[0], state.CM[1]];
+
+printDrift = function() {
+  var dx = state.CM[0] - initialCM[0],
+      dy = state.CM[1] - initialCM[1];
+
+  console.log(format('.2f')(state.time) + ', ' + format('.3f')(dx) + ', ' + format('.3f')(dy));
 };
 
 exports.run = function() {
-  var n = (process.argv.length > 2) ? parseInt(process.argv[2], 10) : Infinity;
+  var n = 200;
 
-  if (isNaN(n)) {
+  // could use commander or optimist to do opt parsing
+  if (process.argv.length > 2) dt = parseFloat(process.argv[2], 10);
+  if (process.argv.length > 3) n = parseInt(process.argv[3], 10);
+
+  if (isNaN(dt)) {
     console.log("couldn't understand \"%s\"", process.argv[2]);
     return;
   }
 
-  printCM();
+  if (isNaN(n)) {
+    console.log("couldn't understand \"%s\"", process.argv[3]);
+    return;
+  }
 
   while (n--) {
-    integrator.integrate();
-    printCM();
+    integrator.integrate(1, dt);
+    printDrift();
   }
 };
