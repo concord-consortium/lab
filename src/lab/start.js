b096f72e5563f54e4f70e9d0c890975eafdbf0cc
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-03-19 18:29:58 -0400

    prevent lack of console.log in IE from causing problems

--- src/lab/start.js
+++ src/lab/start.js
@@ -1,1 +1,9 @@
 (function(){
+
+  // prevent a console.log from blowing things up if we are on a browser that
+  // does not support it
+  if (typeof console === 'undefined') {
+    window.console = {} ;
+    console.log = console.info = console.warn = console.error = function(){};
+  }
+
