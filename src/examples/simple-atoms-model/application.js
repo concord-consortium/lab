b35e66245180631197458ff01e3ca743e8288190
Richard Klancer <rpk@pobox.com>
2012-05-01 17:54:32 -0400

    Save by PUTting to server

--- src/examples/simple-atoms-model/application.js
+++ src/examples/simple-atoms-model/application.js
@@ -7,29 +7,41 @@
 
 (function() {
 
-  var request = $.get('/model-config'),
-      windowLoad = $.Deferred(),
-      controller,
-      opts,
-      modelConfig = {
+  var modelConfig = {
         layoutStyle        : 'simple-static-screen',
+        mol_number         : 50,
         autostart          : false,
         maximum_model_steps: Infinity,
         lj_epsilon_min     : -0.4,
-        lj_epsilon_max     : -0.01034
-      };
+        lj_epsilon_max     : -0.01034,
+        epsilon            : -0.1
+      },
+
+      request = $.get('/model-config'),
+      windowLoad = $.Deferred(),
+
+      controller,
+      opts,
+      _rev;
 
   $(window).load(function() {
     windowLoad.resolve();
 
     $('#save-button').click(function() {
-      var json = model.serialize();
-      alert(json);
+      var props = model.serialize();
+      props._rev = _rev;
+
+      $.ajax('/model-config', {
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(props, 2)
+      });
     });
   });
 
   $.when(request, windowLoad).done(function(xhr) {
     opts = xhr[0];
+    _rev = opts._rev;
   }).fail(function() {
     opts = {
       mol_number          : 50,
