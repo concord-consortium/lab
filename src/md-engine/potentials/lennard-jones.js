ed4c068d349f244525ecb4e640aa5c0f3e771fe9
Richard Klancer <rpk@pobox.com>
2012-05-03 13:29:54 -0400

    Simplify lennardJonesCalculator layout

--- src/md-engine/potentials/lennard-jones.js
+++ src/md-engine/potentials/lennard-jones.js
@@ -1,3 +1,4 @@
+/*jshint eqnull:true boss:true */
 var constants = require('../constants'),
     unit      = constants.unit,
 
@@ -26,16 +27,16 @@ exports.makeLennardJonesCalculator = function(params, cb) {
       alpha_Force,      // units are "MW Force Units" * nm^13
       beta_Force,       // units are "MW Force Units" * nm^7
 
-      coefficients = function(e, s) {
+      setCoefficients = function(e, s) {
         // Input units:
         //  epsilon: eV
         //  sigma:   nm
 
-        if (arguments.length) {
-          epsilon = e;
-          sigma   = s;
-          rmin    = Math.pow(2, 1/6) * sigma;
+        epsilon = e;
+        sigma   = s;
+        rmin    = Math.pow(2, 1/6) * sigma;
 
+        if (epsilon != null && sigma != null) {
           alpha_Potential = 4 * epsilon * Math.pow(sigma, 12);
           beta_Potential  = 4 * epsilon * Math.pow(sigma, 6);
 
@@ -45,75 +46,73 @@ exports.makeLennardJonesCalculator = function(params, cb) {
           beta_Force =  6 * constants.convert(beta_Potential,  { from: unit.EV, to: unit.JOULE }) * NANOMETERS_PER_METER * MW_FORCE_UNITS_PER_NEWTON;
         }
 
-        var coefficients = {
+        if (typeof cb === 'function') cb(getCoefficients(), this);
+      },
+
+      getCoefficients = function() {
+        return {
           epsilon: epsilon,
           sigma  : sigma,
           rmin   : rmin
         };
-
-        if (typeof cb === 'function') cb(coefficients, this);
-
-        return coefficients;
       },
 
-      /**
-        Input units: r_sq: nm^2
-        Output units: eV
-
-        minimum is at r=rmin, V(rmin) = 0
-      */
-      potentialFromSquaredDistance = function(r_sq) {
-         return alpha_Potential*Math.pow(r_sq, -6) - beta_Potential*Math.pow(r_sq, -3) + epsilon;
-      },
-
-      /**
-        Input units: r_sq: nm^2
-        Output units: MW Force Units / nm (= Dalton / fs^2)
-      */
-      forceOverDistanceFromSquaredDistance = function(r_sq) {
-        // optimizing divisions actually does appear to be *slightly* faster
-        var r_minus2nd  = 1 / r_sq,
-            r_minus6th  = r_minus2nd * r_minus2nd * r_minus2nd,
-            r_minus8th  = r_minus6th * r_minus2nd,
-            r_minus14th = r_minus8th * r_minus6th;
-
-        return alpha_Force*r_minus14th - beta_Force*r_minus8th;
-      };
+      // this object
+      calculator;
 
       // Initialize coefficients to passed-in values
-      coefficients(params.epsilon, params.sigma);
+      setCoefficients(params.epsilon, params.sigma);
 
-  return {
+  return calculator = {
 
-    coefficients: coefficients,
+    coefficients: getCoefficients,
 
     setEpsilon: function(e) {
-      return coefficients(e, sigma);
+      setCoefficients(e, sigma);
     },
 
     setSigma: function(s) {
-      return coefficients(epsilon, s);
+      setCoefficients(epsilon, s);
     },
 
-    // "fast" forms which avoid the need for a square root
-    potentialFromSquaredDistance: potentialFromSquaredDistance,
+    /**
+      Input units: r_sq: nm^2
+      Output units: eV
+
+      minimum is at r=rmin, V(rmin) = 0
+    */
+    potentialFromSquaredDistance: function(r_sq) {
+       return alpha_Potential*Math.pow(r_sq, -6) - beta_Potential*Math.pow(r_sq, -3) + epsilon;
+    },
 
     /**
       Input units: r: nm
       Output units: eV
     */
     potential: function(r) {
-      return potentialFromSquaredDistance(r*r);
+      return calculator.potentialFromSquaredDistance(r*r);
     },
 
-    forceOverDistanceFromSquaredDistance: forceOverDistanceFromSquaredDistance,
+    /**
+      Input units: r_sq: nm^2
+      Output units: MW Force Units / nm (= Dalton / fs^2)
+    */
+    forceOverDistanceFromSquaredDistance: function(r_sq) {
+      // optimizing divisions actually does appear to be *slightly* faster
+      var r_minus2nd  = 1 / r_sq,
+          r_minus6th  = r_minus2nd * r_minus2nd * r_minus2nd,
+          r_minus8th  = r_minus6th * r_minus2nd,
+          r_minus14th = r_minus8th * r_minus6th;
+
+      return alpha_Force*r_minus14th - beta_Force*r_minus8th;
+    },
 
     /**
       Input units: r: nm
       Output units: MW Force Units (= Dalton * nm / fs^2)
     */
     force: function(r) {
-      return r * forceOverDistanceFromSquaredDistance(r*r);
+      return r * calculator.forceOverDistanceFromSquaredDistance(r*r);
     }
   };
 };
