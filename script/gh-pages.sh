5e6fec2c011ac4918c176b0205ac39e13a49bb49
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-05-10 11:11:27 -0400

    [#29398303] script for updating gh-pages branch
    uses new top-level dir: script/ for repo management
    and deployment scripts

---
+++ script/gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if git diff --exit-code --quiet && git diff --cached --exit-code --quiet
+then
+  cd dist
+  git pull
+  cd ..; make clean; make
+  cd dist; git add .
+  git commit -m "gh-pages generated from `git --git-dir ../.git log -1 --format=%H`"
+  git push
+  cd ..
+else
+  echo "\n*** please commit or stash changes in your working dir first\n"
+  git status
+fi
