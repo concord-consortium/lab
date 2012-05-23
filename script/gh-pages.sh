439f4bbd29ff3dc7057ff8a978d166aabcbec914
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-05-23 03:10:28 -0400

    small changes to script/gh-pages.sh

--- script/gh-pages.sh
+++ script/gh-pages.sh
@@ -4,10 +4,10 @@ then
   cd dist
   git fetch
   git reset --hard origin/gh-pages
-  cd ..; make clean; make
-  cd dist; git add .
+  cd ..; make clean; make; cd dist
+  git add .
   git commit -am "gh-pages generated from `git --git-dir ../.git log -1 --format=%H`"
-  git push
+  git push origin gh-pages
   cd ..
 else
   echo "\n*** please commit or stash changes in your working dir first\n"
