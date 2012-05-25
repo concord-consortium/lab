7602d8716993208afa7fab3f6840bdf743562454
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-05-25 13:45:36 -0400

    property attributes needed for assignment from hash From: http://www.couchrest.info/model/properties.html  Attributes sent to #attribtues= or #update_attributes that do not have a  property definition will not be updated. This prevents useless data being  passed to database, such as from an HTML form, and also because most projects  will need to do some kind of typecasting to allow the ruby code to understand  the data. However, if you would like truely dynamic attributes (CouchDB is  schema-less after all!), the mass_assign_any_attribute configuration option  when set to true will store everything you put into the mass assignment methods.

--- server/app/models/md2d_model.rb
+++ server/app/models/md2d_model.rb
@@ -1,2 +1,18 @@
 class Md2dModel < CouchRest::Model::Base
+  attr_accessor :casted_by
+
+  property :name,                 String
+
+  property :temperature,          Float
+  property :coulomb_forces,       Float
+  property :epsilon,              Float
+  property :sigma,                Float
+
+  property :lennard_jones_forces, TrueClass
+  property :temperature_control,  TrueClass
+
+  property :atoms,                Hash
+
+  timestamps!
+
 end
