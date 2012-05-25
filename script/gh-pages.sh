6cbbe9f30c0888c36095b38b8fd8a01c6536d993
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-05-25 17:19:39 -0400

    move dist folder to server/public

--- script/gh-pages.sh
+++ script/gh-pages.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if git diff --exit-code --quiet && git diff --cached --exit-code --quiet
 then
-  cd dist
+  cd server/public
   git fetch
   git reset --hard origin/gh-pages
-  cd ..; make clean; make; cd dist
+  cd ..; make clean; make; cd server/public
   git add .
   git commit -am "gh-pages generated from `git --git-dir ../.git log -1 --format=%H`"
   git push origin gh-pages
