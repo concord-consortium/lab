1b469ddec36953a7c1852f2372fa877fc699e4c7
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-06-05 19:38:26 -0400

    Compare Models now lists all comparable models
    [#30146185]
    
    A total of 317 ...
    
    - Potential Tests
    - Sam Activities
    - MW Tutorial/Manual
    
    The file: server/public/imports/legacy-mw-content/model-list.js
    
    Is created after the mw-batch processing by this Ruby script:
    
      src/mw-helpers/post-batch-processor.rb
    
    This is integrated into the Makefile/build process.

---
+++ src/mw-helpers/post-batch-processor.rb
@@ -0,0 +1,94 @@
+require 'json'
+
+def process_tutorial_dir(tutorial_dir)
+  section_path = tutorial_dir
+  all_cml_files =  Dir["#{tutorial_dir}/*.cml"]
+  cml_files = []
+  mml_files = []
+  pages = []
+  all_cml_files.each do |cml_file|
+    index = 0
+    index_str = ""
+    cml_base = cml_file.gsub(".cml", "")
+    name = File.basename(cml_base)
+    matching_mml_files = Dir["#{cml_base}*.mml"]
+    matching_mml_files.each do |mml_file|
+      if matching_mml_files.length > 1
+        index += 1
+        index_str = "-#{index}"
+      end
+      page = {
+        "name" => name + index_str,
+        "path" => section_path + "/",
+        "cml" => cml_file,
+        "mml" => mml_file,
+        "json" => 'converted/' + mml_file.gsub(".mml", ".json")
+      }
+      pages.push(page)
+    end
+  end
+  section = { "section" => section_path }
+  section["content"] = pages
+  pages.length > 0 ? section : false
+end
+
+def process_page_dir(page_dir)
+  section_path = File.dirname(page_dir)
+  name = File.basename(page_dir)
+  cml_file =  Dir["#{page_dir}/*.cml"][0]
+  mml_files = Dir["#{page_dir}/*.mml"]
+  page_index = mml_files.length
+  index = 0
+  index_str = ""
+  pages = []
+  mml_files.each do |mml_file|
+    if mml_files.length > 1
+      index += 1
+      index_str = "-#{index}"
+    end
+    page = {
+      "name" => name + index_str,
+      "path" => section_path + "/",
+      "cml" => cml_file,
+      "mml" => mml_file,
+      "json" => 'converted/' + mml_file.gsub(".mml", ".json")
+    }
+    pages.push(page)
+  end
+  pages
+end
+
+def process_section(dir, special_path="")
+  section_path = dir
+  section = { "section" => section_path }
+  page_directories = Dir["#{dir}/#{special_path}**"]
+  page_directories = page_directories.find_all { |d| Dir["#{d}/*.mml"].length > 0 }
+  section["content"] = []
+  page_directories.each do |page_dir|
+    section["content"] += process_page_dir(page_dir)
+  end
+  section
+end
+
+model_list = []
+path = "server/public/imports/legacy-mw-content"
+
+Dir.chdir(path) do
+  dirs = Dir["sam-activities/**"]
+  dirs.each do |dir|
+    model_list.push(process_section(dir, "original-interactives-in-pages/"))
+  end
+  model_list.push(process_section("potential-tests"))
+  model_list.push(process_tutorial_dir("tutorial"))
+  dirs = Dir["tutorial/**"]
+  dirs.each do |dir|
+    tutorial_section = process_tutorial_dir(dir)
+    model_list.push(tutorial_section) if tutorial_section
+  end
+end
+
+model_list_dot_js = "#{path}/model-list.js"
+
+File.open(model_list_dot_js, "w") {|f| f.write("modelList = " + JSON.pretty_generate(model_list)) }
+
+puts "created: #{model_list_dot_js}"
