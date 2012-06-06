c82edd4f0c0e4ec529211fd7c94b938a0d18fab5
Richard Klancer <rpk@pobox.com>
2012-06-05 23:10:50 -0400

    Good-enough gradient descent with bounds checking

--- src/md-engine/math/index.js
+++ src/md-engine/math/index.js
@@ -1,2 +1,5 @@
 exports.normal              = require('./distributions').normal;
 exports.getWindowedAverager = require('./utils').getWindowedAverager;
+exports.minimize            = require('./minimizer').minimize;
+
+if (window) window.minimize = exports.minimize;
