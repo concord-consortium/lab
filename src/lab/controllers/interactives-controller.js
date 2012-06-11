90e174ac0f2cd3ad35d44780c21e88a722e0cc4e
Sam Fentress <sfentress@concord.org>
2012-06-11 09:19:32 -0400

    reformat indentation
    (Did this in two commits to make previous change clearer)

--- src/lab/controllers/interactives-controller.js
+++ src/lab/controllers/interactives-controller.js
@@ -10,76 +10,76 @@ controllers.interactivesController = function(interactive, interactive_view_id) 
       $interactiveContainer,
       i, ii;
 
-    function loadModel(modelUrl) {
-      var playerConfig = {    // to be removed
-          layoutStyle        : 'simple-static-screen',
-          maximum_model_steps: Infinity
-        };
-      $.get(modelUrl).done(function(modelConfig) {
-        if (simpleController) {
-          simpleController.reload(modelConfig, playerConfig);
-        } else {
-          simpleController = controllers.simpleModelController('#molecule-container', modelConfig, playerConfig);
-        }
-      });
-    }
-
-    function createComponent(component) {
-      switch (component.type) {
-        case "button":
-          return createButton(component);
-      }
-    }
-
-    function createButton(component) {
-      var $button, scriptStr, script;
-
-      $button = $("<button>").html(component.text);
-
-      if (typeof component.action === "string") {
-        scriptStr = component.action;
+  function loadModel(modelUrl) {
+    var playerConfig = {    // to be removed
+        layoutStyle        : 'simple-static-screen',
+        maximum_model_steps: Infinity
+      };
+    $.get(modelUrl).done(function(modelConfig) {
+      if (simpleController) {
+        simpleController.reload(modelConfig, playerConfig);
       } else {
-        scriptStr = component.action.join('\n');
+        simpleController = controllers.simpleModelController('#molecule-container', modelConfig, playerConfig);
       }
-      eval("script = function() {"+scriptStr+"}");
-      $button.click(script);
+    });
+  }
 
-      return $button;
+  function createComponent(component) {
+    switch (component.type) {
+      case "button":
+        return createButton(component);
+    }
+  }
+
+  function createButton(component) {
+    var $button, scriptStr, script;
+
+    $button = $("<button>").html(component.text);
+
+    if (typeof component.action === "string") {
+      scriptStr = component.action;
+    } else {
+      scriptStr = component.action.join('\n');
+    }
+    eval("script = function() {"+scriptStr+"}");
+    $button.click(script);
+
+    return $button;
+  }
+
+  function loadInteractive(newInteractive, interactive_view_id) {
+    interactive = newInteractive;
+    $interactiveContainer = $(interactive_view_id);
+    if ($interactiveContainer.children().length === 0) {
+      $interactiveContainer.append('<div id="molecule-container"/>');
+      $interactiveContainer.append('<div id="bottom"/>');
+    } else {
+      $(bottom).remove();
+      $interactiveContainer.append('<div id="bottom"/>');
     }
 
-    function loadInteractive(newInteractive, interactive_view_id) {
-      interactive = newInteractive;
-      $interactiveContainer = $(interactive_view_id);
-      if ($interactiveContainer.children().length === 0) {
-        $interactiveContainer.append('<div id="molecule-container"/>');
-        $interactiveContainer.append('<div id="bottom"/>');
-      } else {
-        $(bottom).remove();
-        $interactiveContainer.append('<div id="bottom"/>');
-      }
-
-      if (interactive.model) {
-        loadModel(interactive.model);
-      }
-
-      components = interactive.components;
-
-      for (i = 0, ii=components.length; i<ii; i++) {
-        component = createComponent(components[i]);
-        $('#bottom').append(component);
-      }
+    if (interactive.model) {
+      loadModel(interactive.model);
     }
 
-    function updateLayout() {
-      simpleController.updateLayout();
+    components = interactive.components;
+
+    for (i = 0, ii=components.length; i<ii; i++) {
+      component = createComponent(components[i]);
+      $('#bottom').append(component);
     }
+  }
 
-    // run this when controller is created
-    loadInteractive(interactive, interactive_view_id);
+  function updateLayout() {
+    simpleController.updateLayout();
+  }
 
-    // make these private variables and functions available
-    controller.loadInteractive = loadInteractive;
-    controller.updateLayout = updateLayout;
+  // run this when controller is created
+  loadInteractive(interactive, interactive_view_id);
+
+  // make these private variables and functions available
+  controller.loadInteractive = loadInteractive;
+  controller.updateLayout = updateLayout;
 
   return controller;
 };
