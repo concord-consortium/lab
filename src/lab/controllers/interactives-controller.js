3c9c2156ac7ead621115babcf9b5c54584dd0f0e
Sam Fentress <sfentress@concord.org>
2012-06-12 15:15:31 -0400

    Create start of interactives layout system
    An interactive can specify a 'layout' property that
    will (for now) simply add the specified views to the
    element with that id.

--- src/lab/controllers/interactives-controller.js
+++ src/lab/controllers/interactives-controller.js
@@ -1,20 +1,17 @@
-/*globals controllers $ */
+/*globals controllers model layout Thermometer $ */
 
 /*jslint onevar: true*/
 controllers.interactivesController = function(interactive, interactive_view_id) {
 
   var controller = {},
-      components,
-      component,
       simpleController,
       $interactiveContainer,
       propertiesListeners = [],
-      actionQueue = [],
-      i, ii;
+      actionQueue = [];
 
   function loadModel(modelUrl) {
     var playerConfig = {    // to be removed
-        layoutStyle        : 'simple-static-screen',
+        layoutStyle        : 'interactive',
         maximum_model_steps: Infinity
       };
     $.get(modelUrl).done(function(modelConfig) {
@@ -39,7 +36,7 @@ controllers.interactivesController = function(interactive, interactive_view_id) 
   function createButton(component) {
     var $button, scriptStr, script;
 
-    $button = $("<button>").html(component.text);
+    $button = $("<button>").attr('id', component.id).html(component.text);
 
     if (typeof component.action === "string") {
       scriptStr = component.action;
@@ -53,8 +50,11 @@ controllers.interactivesController = function(interactive, interactive_view_id) 
   }
 
   function createThermometer(component) {
-    var $therm = $('<div id="thermometer"/>'),
-        thermometer = new Thermometer($therm, 0, component.min, component.max);
+    var $therm = $('<div>').attr('id', component.id),
+        thermometer = new Thermometer($therm, 0, component.min, component.max),
+        $wrapper = $('<div>').css("padding-bottom", "4em")
+          .append($therm)
+          .append($('<div>').text("Thermometer"));
 
     function updateTherm() {
       thermometer.add_value(model.get("temperature"));
@@ -65,12 +65,14 @@ controllers.interactivesController = function(interactive, interactive_view_id) 
       thermometer.resize();
       updateTherm();
     });
-    return $therm;
+
+    layout.addView('thermometers', thermometer);
+    return $wrapper;
   }
 
   function queuePropertiesListener(properties, func) {
     if (typeof model !== "undefined") {
-      model.addPropertiesListener(properties, func)
+      model.addPropertiesListener(properties, func);
     } else {
       propertiesListeners.push([properties, func]);
     }
@@ -85,6 +87,9 @@ controllers.interactivesController = function(interactive, interactive_view_id) 
   }
 
   function modelLoaded() {
+    var listener,
+        action;
+
     while (propertiesListeners.length > 0) {
       listener = propertiesListeners.pop();
       model.addPropertiesListener(listener[0], listener[1]);
@@ -95,15 +100,27 @@ controllers.interactivesController = function(interactive, interactive_view_id) 
   }
 
   function loadInteractive(newInteractive, interactive_view_id) {
+    var componentJsons,
+        components = {},
+        component,
+        divArray,
+        div,
+        componentId,
+        $top, $right,
+        i, ii;
+
     interactive = newInteractive;
     $interactiveContainer = $(interactive_view_id);
     if ($interactiveContainer.children().length === 0) {
-      $top = $('<div id="top"/>');
+      $top = $('<div class="top" id="top"/>');
       $top.append('<div id="molecule-container"/>');
+      $right = $('<div id="right"/>');
+      $top.append($right);
       $interactiveContainer.append($top);
-      $interactiveContainer.append('<div id="bottom"/>');
+      $interactiveContainer.append('<div class="bottom" id="bottom"/>');
     } else {
-      $('#bottom').remove();
+      $('#bottom').html('');
+      $('#right').html('');
       $interactiveContainer.append('<div id="bottom"/>');
     }
 
@@ -111,12 +128,40 @@ controllers.interactivesController = function(interactive, interactive_view_id) 
       loadModel(interactive.model);
     }
 
-    components = interactive.components;
+    componentJsons = interactive.components;
 
-    for (i = 0, ii=components.length; i<ii; i++) {
-      component = createComponent(components[i]);
-      $('#bottom').append(component);
+    for (i = 0, ii=componentJsons.length; i<ii; i++) {
+      component = createComponent(componentJsons[i]);
+      components[componentJsons[i].id] = component;
     }
+
+
+    // look at each div defined in layout, and add any components in that
+    // array to that div. Then rm the component from components so we can
+    // add the remainder to #bottom at the end
+    if (interactive.layout) {
+      for (div in interactive.layout) {
+        if (interactive.layout.hasOwnProperty(div)) {
+          divArray = interactive.layout[div];
+          for (i = 0, ii = divArray.length; i<ii; i++) {
+            componentId = divArray[i];
+            if (components[componentId]) {
+              $('#'+div).append(components[componentId]);
+              delete components[componentId];
+            }
+          }
+        }
+      }
+    }
+
+    // add the remaining components to #bottom
+    for (componentId in components) {
+      if (components.hasOwnProperty(componentId)) {
+        $('#bottom').append(components[componentId]);
+      }
+    }
+
+
   }
 
   function updateLayout() {
