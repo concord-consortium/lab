8d8af7d6ea514b870e6026736c3d70781297f2cb
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-06-17 22:20:09 -0400

    use correct global name: ACTUAL_ROOT

--- src/examples/energy2d-model/application.js
+++ src/examples/energy2d-model/application.js
@@ -11,7 +11,7 @@
   var
     ROOT = "/examples",
     ROOT_REGEX = new RegExp(ROOT + "/.*$"),
-    REAL_ROOT = document.location.pathname.replace(ROOT_REGEX, ''),
+    ACTUAL_ROOT = document.location.pathname.replace(ROOT_REGEX, ''),
     DEFAULT_INTERACTIVE = "benard_cell",
     window_loaded = $.Deferred(),
     options_loaded = $.Deferred(),
