abb4ba0ed85145b905caf62b75241f2bf5a2e0ae
Richard Klancer <rpk@pobox.com>
2012-06-18 17:19:04 -0400

    minimize doesn't need to be global either

--- src/lab/models/md2d/engine/math/index.js
+++ src/lab/models/md2d/engine/math/index.js
@@ -1,5 +1,3 @@
 exports.normal              = require('./distributions').normal;
 exports.getWindowedAverager = require('./utils').getWindowedAverager;
 exports.minimize            = require('./minimizer').minimize;
-
-if (window) window.minimize = exports.minimize;
