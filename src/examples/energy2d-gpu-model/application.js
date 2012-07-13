bba6c437c02787135b33e1aeef4c732c033db2c9
Piotr Janik <janikpiotrek@gmail.com>
2012-07-13 20:06:50 +0200

    Add a new Energy2D example with WebGL support

---
+++ src/examples/energy2d-gpu-model/application.js
@@ -0,0 +1,75 @@
+/*globals energy2d, document interactivesIndex window location $ */
+/*jslint indent: 2 */
+// ------------------------------------------------------------
+//
+// Energy2D Demo
+//
+// ------------------------------------------------------------
+
+var ROOT = "/examples",
+    ROOT_REGEX = new RegExp(ROOT + "/.*$"),
+    ACTUAL_ROOT = document.location.pathname.replace(ROOT_REGEX, '');
+
+(function () {
+  'use strict';
+  var
+    DEFAULT_INTERACTIVE = "fixed-flux-boundary512",
+    window_loaded = $.Deferred(),
+    options_loaded = $.Deferred(),
+    hash,
+    interactive_url,
+    interactive_options,
+    controller,
+    select = $("#select-interactive"),
+    force_WebGL_checkbox = $('#enable-WebGL'),
+
+    loadInteractive = function () {
+
+    };
+
+  $.each(interactivesIndex, function (key, value) {
+    select.append($("<option>")
+      .attr('value', key)
+      .text(value.name)
+      .attr('data-path', value.path));
+  });
+
+  hash = document.location.hash || "#" + interactivesIndex[DEFAULT_INTERACTIVE].path;
+  document.location.hash = hash;
+  interactive_url = hash.substr(1, hash.length);
+
+  select.find("option[data-path='" + interactive_url + "']").attr('selected', true);
+
+  $.get(interactive_url).done(function (results) {
+    if (typeof results === "string") { results = JSON.parse(results); }
+    interactive_options = results;
+    options_loaded.resolve();
+  });
+
+  $(window).load(function () {
+    window_loaded.resolve();
+  });
+
+  $.when(window_loaded, options_loaded).done(function () {
+    controller = energy2d.controllers.makeInteractiveController(
+      interactive_options, 
+      '#interactive-container', 
+      '#interactive-description'
+    );
+  });
+
+  select.change(function (option) {
+    document.location.hash = "#" + $(select.find("option:selected")[0]).data('path');
+  });
+
+  force_WebGL_checkbox.change(function(){
+    controller.setWebGLEnabled($(this).attr("checked"));
+  });
+
+  $(window).bind('hashchange', function () {
+    if (document.location.hash !== hash) {
+      location.reload();
+    }
+  });
+
+}());
