731f9ad1c0422727b0ed1cb7e70902af47a8dd50
Richard Klancer <rpk@pobox.com>
2012-07-20 15:17:53 -0400

    Exhaustively explain minimal inheritance pattern.

--- src/experiments/inquiry-space-importer/js/app.js
+++ src/experiments/inquiry-space-importer/js/app.js
@@ -3,8 +3,7 @@
 if (typeof ISImporter === 'undefined') ISImporter = {};
 
 /**
-  Helpers for defining classical-OO style inheritance.
-  Adapted from CoffeeScript implementation.
+  Helpers for defining classical-OO style inheritance, adapted from CoffeeScript.
 */
 (function() {
 
@@ -17,35 +16,77 @@ if (typeof ISImporter === 'undefined') ISImporter = {};
     }
   }
 
+  // Remember that "classes" are just constructor functions that create objects, and that the
+  // constructor function property called `prototype` is used to define the prototype object
+  // (aka the __proto__ property) which will be assigned to instances created by the constructor.
+  // Properties added to the prototype object of a constructor effectively become the instance
+  // properties/methods of objects created with that constructor, and properties of the prototype
+  // of the prototype are effectively "superclass" instance properties/methods.
+  //
+  // See http://javascriptweblog.wordpress.com/2010/06/07/understanding-javascript-prototypes/
 
-  function extend(child, parent) {
-    mixin(child, parent);
+  /**
+    Assuming Child, Parent are classes (i.e., constructor functions):
+      1. Copies the properties of the Parent constructor to the Child constructor (These can be
+         considered "class properties"/methods, shared among all instances of a class.)
+      2. Adds Parent's prototype to Child's prototype chain.
+      3. Adds Parent's prototype to the '__super__' property of Child.
+  */
+  function extend(Child, Parent) {
 
-    function Ctor() {
-      this.constructor = child;
+    // First, copy direct properties of the constructor object ("class properties") from Parent to
+    // Child.
+    mixin(Child, Parent);
+
+    // First step in extending the prototype chain: make a throwaway constructor, whose prototype
+    // property is the same as the Parent constructor's prototype property. Objects created by
+    // calling `new PrototypeConstructor()` will have the *same* prototype object as objects created
+    // by calling `new Parent()`.
+    function PrototypeConstructor() {
+      this.constructor = Child;
     }
-    Ctor.prototype  = parent.prototype;
-    child.prototype = new Ctor();
-    child.__super__ = parent.prototype;
+    PrototypeConstructor.prototype = Parent.prototype;
+
+    // Now use PrototypeConstructor to extend the prototype chain by one link.
+    // That is, use PrototypeConstructor to make a new *object* whose prototype object
+    // (__proto__ property) is Parent.prototype, and assign the object to the Child constructor's
+    // prototype property. This way, objects created by calling "new Child()"
+    // will have a prototype object whose prototype object in turn is Parent.prototype.
+    Child.prototype = new PrototypeConstructor();
+
+    // Assign the prototype used by objects created by Parent to the __super__ property of Child.
+    // (This property can be accessed within a Child instance as `this.constructor.__super__`.)
+    // This allows a Child instance to look "up" the prototype chain to find instances properties
+    // defined in Parent that are overridden in Child (i.e., defined on Child.prototype)
+    Child.__super__ = Parent.prototype;
   }
 
-
-  function defineClass(classProperties) {
-    var ctor = function(instanceProperties) {
+  /**
+    Defines a "class" whose instances will have the properties defined in `prototypeProperties`:
+      1. Creates a new constructor, which accepts a list of properties to be copied directly onto
+         the instance returned by the constructor.
+      2. Adds the properties in `prototypeProperties` to the prototype object shared by instances
+         created by the constructor.
+  */
+  function defineClass(prototypeProperties) {
+    function NewConstructor(instanceProperties) {
        mixin(this, instanceProperties);
-    };
-    mixin(ctor.prototype, classProperties);
-    return ctor;
+    }
+    mixin(NewConstructor.prototype, prototypeProperties);
+    return NewConstructor;
   }
 
-
-  function extendClass(baseClass, classProperties) {
-    var ctor = function(instanceProperties) {
-          mixin(this, instanceProperties);
-        };
-    extend(ctor, baseClass);
-    mixin(ctor.prototype, classProperties);
-    return ctor;
+  /**
+    Given ParentClass, return a new class which is ParentClass extended by childPrototypeProperties
+  */
+  function extendClass(ParentClass, childPrototypeProperties) {
+    function ChildConstructor(instanceProperties) {
+      mixin(this, instanceProperties);
+    }
+    // Extend ParentClass first so childPrototypeProperties override anything defined in ParentClass
+    extend(ChildConstructor, ParentClass);
+    mixin(ChildConstructor.prototype, childPrototypeProperties);
+    return ChildConstructor;
   }
 
   // publish defineClass, extendClass globally:
