735ce850db298080a2d88efb07f400836b33e768
Richard Klancer <rpk@pobox.com>
2012-07-25 12:49:00 -0400

    Use new SensorApplet classes in inquiry-space-importer

--- src/experiments/inquiry-space-importer/js/app.js
+++ src/experiments/inquiry-space-importer/js/app.js
@@ -2,38 +2,21 @@
 
 if (typeof ISImporter === 'undefined') ISImporter = {};
 
-/**
-  Base SensorApplet class appends the appropriate HTML to the DOM.
-*/
-ISImporter.SensorApplet = defineClass({
-  doStuff1: function() {
-    console.log("doing stuff 1 as SensorApplet");
-  },
-  doStuff2: function() {
-    console.log("doing stuff 2 as SensorApplet");
-  }
-});
-
-ISImporter.GoIOApplet = extendClass(ISImporter.SensorApplet, {
-  doStuff1: function() {
-    console.log("doing stuff 1 as GoIOApplet");
-    this.constructor.__super__.doStuff1.apply(this, arguments);
-  }
-});
-
-
 ISImporter.main = function() {
 
   distanceSensor = new ISImporter.GoIOApplet({
-    otmlString: 'distance'
+    otmlPath: '/distance.otml',
+    listenerPath: 'distanceSensor'
   }),
 
   temperatureSensor = new ISImporter.GoIOApplet({
-    otmlString: 'temperature'
+    otmlPath: '/temperature.otml',
+    listenerPath: 'temperatureSensor'
   }),
 
   lightSensor = new ISImporter.GoIOApplet({
-    otmlString: 'light'
+    otmlPath: '/light.otml',
+    listenerPath: 'lightSensor'
   });
 
 };
