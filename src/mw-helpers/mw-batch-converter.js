98e78523b4759be52b0b10effba464682f0d85c6
Richard Klancer <rpk@pobox.com>
2012-08-09 11:01:03 -0400

    Allow mw batch converter to check for outdated files

--- src/mw-helpers/mw-batch-converter.js
+++ src/mw-helpers/mw-batch-converter.js
@@ -1,3 +1,5 @@
+/*jshint eqnull:true */
+
 /**
 For the moment, to run these scripts
   $ cd lab
@@ -8,107 +10,162 @@ For the moment, to run these scripts
 **/
 require('coffee-script');
 
-var parseMML = require('./mml-parser'),
+var parseMML = require('./mml-parser').parseMML,
     mkdirp = require('mkdirp'),
     fs = require('fs'),
     path = require('path'),
     jade = require('jade'),
     util = require('util'),
+
     rootPath = require.main ? path.dirname(require.main.filename) : process.cwd(),
     legacyFolderPath = path.normalize(rootPath + '/../imports/legacy-mw-content/'),
     convertedFolderPath = path.normalize(rootPath + '/../server/public/imports/legacy-mw-content/converted/'),
     templatePath = legacyFolderPath + 'legacyMMLRunnables.jade';
 
-// example mmlFileName:
-// 'sam-activities/chemical-reactions/original-interactives-in-pages/page1/page1$0.mml'
-convertMMLFile = function(mmlFileName){
-  var mml = fs.readFileSync(legacyFolderPath + mmlFileName).toString(),
-      modelName = /\/?([^\/]*)\.mml/.exec(mmlFileName)[1],
+/**
+  Converts one MML file, assuming a directory tree approach.
 
-      // convert model
-      conversion = parseMML.parseMML(mml),
+  @param inputBaseDir  name of root of the directory tree where mml files are found
+  @param mmlPath       path to mml file to convert, relative to inputBaseDir
+  @param outputBaseDir root of directory tree to which mml files are to be put
+  @param onlyOutdated  if true, check mtimes: conversion proceeds only if the output file is not
+                          older than the input file. Default is false.
 
-      folders = mmlFileName.split('/'),
-      outputPath = convertedFolderPath + mmlFileName.match(/.*\//)[0],
-      outputFile;
+  Reads .mml file at path inputBaseDir/mmlPath
+  Writes .json file at outputBaseDir/mmlPath (with '.json' substituted for '.mml')
 
-  if (conversion.json) {
-    mkdirp.sync(outputPath);
+  @returns true if a conversion was made, false otherwise
+  @throws Error if the input file could not be found, or could not be converted.
+*/
+function convertMMLFile(inputBaseDir, mmlPath, outputBaseDir, onlyOutdated) {
 
-    // write model out to file. Overwrites any existing file with the same name
-    outputFile = outputPath+modelName+'.json';
-    fs.writeFileSync(outputFile, JSON.stringify(conversion.json, null, 2));
-    return outputFile;
-  } else {
-    console.log("Error converting "+mmlFileName);
-    console.log("  " + conversion.error);
-    return false;
+  var inputPath  = path.normalize( path.join(inputBaseDir, mmlPath) ),
+      outputPath = path.normalize( path.join(outputBaseDir, mmlPath) ).replace(/mml$/, 'json'),
+
+      inputMtime,
+      outputMtime,
+      mml,
+      conversion;
+
+  if ( !fs.existsSync(inputPath) ) {
+    throw new Error("convertMMLFile: could not find input file \"" + inputPath + "\"");
   }
-};
 
-// recursively finds and collects all mml files in a directory
-// @param directory e.g. './imports/legacy-mw-content/'
-collectAllMMLFiles = function(directory) {
-  function addFiles(dir) {
-    var mmlFiles = [];
-    try {
-      var stat = fs.lstatSync(dir);
-      if (stat.isFile() && ~dir.indexOf(".mml")) {
-        mmlFiles.push(dir);
-      } else if (stat.isDirectory()) {
-        var files = fs.readdirSync(dir);
-        for (var i=0, ii=files.length; i<ii; i++) {
-          var file = files[i];
-          if (file) {
-            mmlFiles = mmlFiles.concat(addFiles(path.join(dir, file)));
-          }
-        }
+  // check existence and mtimes
+  if (onlyOutdated) {
+    if (fs.existsSync(outputPath)) {
+      inputMtime  = fs.statSync(inputPath).mtime,
+      outputMtime = fs.statSync(outputPath).mtime;
+
+      if (inputMtime.getTime() < outputMtime.getTime()) {
+        return false;
       }
     }
-    catch (e) {
-
-    }
-    return mmlFiles;
   }
 
-  return addFiles(directory);
-};
+  // if we got this far, convert the mml file
+  mml = fs.readFileSync(inputPath).toString();
+  conversion = parseMML(mml);
 
-// Batch comverts the entire legacy MML folder
-convertMMLFolder = function(){
-  var mmlFiles = collectAllMMLFiles(legacyFolderPath),
-      successes = 0,
-      i, ii;
-  for (i=0, ii=mmlFiles.length; i<ii; i++) {
-    var relFileName = mmlFiles[i].replace(legacyFolderPath.replace('./',''), '');
-    var success = convertMMLFile(relFileName);
-    if (success) {
-      successes++;
+  if ( conversion.error ) {
+    throw new Error("convertMMLFile: could not convert input file \"" + inputPath + "\"; error = " + conversion.error);
+  }
+
+  // and write the ouptut
+  mkdirp.sync( path.dirname(outputPath) );
+  fs.writeFileSync( outputPath, JSON.stringify(conversion.json, null, 2) );
+  return true;
+}
+
+
+/**
+   Recursively collects all mml files in a directory.
+
+   @param searchPath The path to search. May be a directory or regular file.
+   @param baseDir    Base directory name relative to which all paths will be normalized.
+
+   if baseDir is null, assumes 'searchPath' is the baseDir
+
+   @returns Array of mml file paths found starting at searchPath, named relative to baseDir.
+*/
+function collectAllMMLFiles(searchPath, baseDir) {
+
+  // Search relative to the input path if baseDir is not specified
+  if (baseDir == null) baseDir = searchPath;
+
+  var mmlFiles = [],
+      stat,
+      files,
+      i;
+
+  // statSync follows symlinks, unlike lstatSync
+  stat = fs.statSync(searchPath);
+
+  if (stat.isFile() && path.extname(searchPath) === '.mml') {
+    return [path.relative(baseDir, searchPath)];
+  }
+
+  if (stat.isDirectory()) {
+    files = fs.readdirSync(searchPath);
+    for (i=0; i<files.length; i++) {
+      if (files[i]) {
+        mmlFiles = mmlFiles.concat( collectAllMMLFiles(path.join(searchPath, files[i]), baseDir) );
+      }
     }
   }
 
-  console.log("");
-  console.log("Converted "+successes+" files, failed to convert "+(mmlFiles.length - successes)+" files.");
   return mmlFiles;
-};
+}
 
-// This assumes that convertMMLFolder() has already run. It searches for all the cml files,
-// finds all the related json model files, and creates a web page that has runnable links
-// for both of them
-// @param outputFile = e.g. './server/public/imports/legacy-mw-content/legacyMMLRunnables.html'
-createCmlJsonIndex = function(outputFile) {
+
+/**
+  Batch converts the entire legacy MML folder.
+
+  @param onlyOutdated
+    Whether to only convert outdated MML files, or convert them all
+
+  @returns The number of files converted.
+*/
+function convertMMLFolder(onlyOutdated) {
+
+  var mmlFiles = collectAllMMLFiles(legacyFolderPath),
+      converted,
+      nConverted = 0,
+      i;
+
+  for (i=0; i < mmlFiles.length; i++) {
+    converted = convertMMLFile(legacyFolderPath, mmlFiles[i], convertedFolderPath, !!onlyOutdated);
+    if (converted) nConverted++;
+  }
+
+  return nConverted;
+}
+
+
+/**
+  This assumes that convertMMLFolder() has already run. It searches for all the cml files,
+  finds all the related json model files, and creates a web page that has runnable links
+  for both of them
+  @param outputFile = e.g. './server/public/imports/legacy-mw-content/legacyMMLRunnables.html'
+*/
+function createCmlJsonIndex(outputFile) {
+
   var mmlFiles = collectAllMMLFiles(legacyFolderPath),
       cmlToJsonHash = {},
-      template, jadeFn, html;
+      template, jadeFn, html,
+      i,
+      j;
 
-  for (var i=0, ii=mmlFiles.length; i<ii; i++) {
-    var mmlFolderPath = path.dirname(mmlFiles[i]),
+  for (i = 0; i < mmlFiles.length; i++) {
+
+    var mmlFolderPath = path.normalize( path.join( legacyFolderPath, path.dirname( mmlFiles[i] ) ) ),
         files = fs.readdirSync(mmlFolderPath),
         cmlFiles = [],
-        cmlFile, cmlFilePath;
-    for (var j=0, jj=files.length; j<jj; j++) {
-      if (~files[j].indexOf('.cml')) {
-        cmlFiles.push(files[j]);
+        cmlFilePath;
+
+    for (j = 0; j < files.length; j++) {
+      if (path.extname( files[j] ) === '.cml') {
+        cmlFiles.push( files[j] );
       }
     }
 
@@ -139,9 +196,9 @@ createCmlJsonIndex = function(outputFile) {
 
   console.log("Created index at "+outputFile);
   return cmlToJsonHash;
-};
+}
 
-exports.convertMMLFile = convertMMLFile;
-exports.convertMMLFolder = convertMMLFolder;
+exports.convertMMLFile     = convertMMLFile;
+exports.convertMMLFolder   = convertMMLFolder;
 exports.createCmlJsonIndex = createCmlJsonIndex;
 exports.collectAllMMLFiles = collectAllMMLFiles;
