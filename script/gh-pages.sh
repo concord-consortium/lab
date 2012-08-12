3064cfb44337f14d255a68be4188673fe9c6688d
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-08-12 13:24:42 -0400

    check for absence of gh-pages setup

--- script/gh-pages.sh
+++ script/gh-pages.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
-if git diff --exit-code --quiet && git diff --cached --exit-code --quiet
+if [ ! -d server/public/.git ]
 then
-  cd server/public
-  git fetch
-  git reset --hard origin/gh-pages
-  cd ../..; make clean; make; cd server/public
-  git add .
-  git commit -am "gh-pages generated from `git --git-dir ../../.git log -1 --format=%H`"
-  git push origin gh-pages
-  cd ../..
-else
-  echo "\n*** please commit or stash changes in your working dir first\n"
-  git status
+cat <<heredoc
+
+*** dir server/public not a git repository
+
+    please setup the server/public as a separate git checkout tracking the gh-pages branch first
+    see: http://lab.dev.concord.org/readme.html#making-the-serverpublic-folder-track-the-gh-pages-branch
+
+heredoc
+exit
 fi
+if git diff --exit-code --quiet && git diff --cached --exit-code --quiet
+cat <<heredoc
+
+*** uncommitted changes in your working directory
+
+    please commit or stash changes in your working dir before updating the gh-pages branch
+
+heredoc
+git status
+exit
+fi
+cd server/public
+git fetch
+git reset --hard origin/gh-pages
+cd ../..; make clean; make; cd server/public
+git add .
+git commit -am "gh-pages generated from `git --git-dir ../../.git log -1 --format=%H`"
+git push origin gh-pages
+cd ../..
