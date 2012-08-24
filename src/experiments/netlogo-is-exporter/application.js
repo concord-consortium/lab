1ed13a6ec894ad184e8b9862b9d199cf306812f3
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-08-24 16:46:40 -0400

    Experiment: NetLogo Interactives for DataGames

---
+++ src/experiments/netlogo-is-exporter/application.js
@@ -0,0 +1,97 @@
+/*globals $ CodeMirror controllers model alert DEVELOPMENT: true */
+/*jshint boss:true */
+
+DEVELOPMENT = true;
+
+var ROOT = "/experiments",
+    ROOT_REGEX = new RegExp(ROOT + "/.*$"),
+    ACTUAL_ROOT = document.location.pathname.replace(ROOT_REGEX, '');
+
+(function() {
+
+  var interactiveDefinitionLoaded = $.Deferred(),
+      windowLoaded = $.Deferred(),
+
+      selectInteractive = document.getElementById('select-interactive'),
+
+      editor,
+      controller,
+      indent = 2,
+      interactiveUrl,
+      interactive,
+      hash,
+      jsonModelPath, contentItems, mmlPath, cmlPath,
+      viewType,
+      dgPaylod, dgUrl;
+
+  if (!document.location.hash) {
+    if (selectInteractive) {
+      selectInteractiveHandler();
+    } else {
+      document.location.hash = '#interactives/IS-harmonic-motion-model.json';
+    }
+  }
+
+  if (hash = document.location.hash) {
+    interactiveUrl = hash.substr(1, hash.length);
+
+    $.get(interactiveUrl).done(function(results) {
+      if (typeof results === 'string') results = JSON.parse(results);
+      interactive = results;
+
+      // Use the presense of selectInteractive as a proxy indicating that the
+      // rest of the elements on the non-iframe-embeddable version of the page
+      // are present and should be setup.
+      if (selectInteractive) {
+        setupFullPage();
+      } else {
+        viewType = 'interactive-iframe';
+      }
+
+      interactiveDefinitionLoaded.resolve();
+    });
+  }
+
+  $(window).load(function() {
+    windowLoaded.resolve();
+  });
+
+  $.when(interactiveDefinitionLoaded, windowLoaded).done(function(results) {
+    // controller = controllers.interactivesController(interactive, '#interactive-container', viewType);
+  });
+
+  $(window).bind('hashchange', function() {
+    if (document.location.hash !== hash) {
+      location.reload();
+    }
+  });
+
+  //
+  // The following functions are only used when rendering the
+  // non-embeddable Interactive page
+  //
+  function selectInteractiveHandler() {
+    document.location.hash = '#' + selectInteractive.value;
+  }
+
+  function setupFullPage() {
+    selectInteractive.value = interactiveUrl;
+
+    // construct link to embeddable version of Interactive
+    $("#embeddable-link").attr("href", function(i, href) { return href + hash; });
+
+    // construct link to DataGames embeddable version of Interactive
+    $("#datagames-link").attr("href", function(i, href) {
+      dgPayload = [{
+        "name": $(selectInteractive).find("option:selected").text(),
+        "dimensions": interactive.model.viewOptions.dimensions,
+        "url": "DataGames/Games/concord-lab" + "/examples/interactives/embeddable.html#" +  interactiveUrl
+      }];
+      dgUrl = "http://is.kcptech.com/dg?moreGames=" + JSON.stringify(dgPayload);
+      return encodeURI(dgUrl)
+    });
+
+    selectInteractive.onchange = selectInteractiveHandler;
+  }
+
+}());
