da2dc19c91b42479f763beea5a841b2c9c75a3ab
Sam Fentress <sfentress@concord.org>
2012-09-04 10:23:44 -0400

    Add friction to data table

--- src/examples/interactives/application.js
+++ src/examples/interactives/application.js
@@ -268,7 +268,7 @@ var ROOT = "/examples",
         timeFormatter = d3.format("5.0f"),
         timePrefix = "",
         timeSuffix = " (fs)",
-        column_titles = ['PX', 'PY', 'X', 'Y', 'VX', 'VY', 'AX', 'AY', 'SPEED', 'CHARGE', 'RADIUS', 'ELEMENT'],
+        column_titles = ['PX', 'PY', 'X', 'Y', 'VX', 'VY', 'AX', 'AY', 'SPEED', 'CHARGE', 'RADIUS', 'FRICTION', 'ELEMENT'],
         i_formatter = d3.format(" 2d"),
         charge_formatter = d3.format(" 1.1f"),
         radius_formatter = d3.format(" 1.2f"),
@@ -278,7 +278,7 @@ var ROOT = "/examples",
         formatters = [f_formatter, f_formatter, f_formatter,
                       f_formatter, e_formatter, e_formatter,
                       e_formatter, e_formatter, e_formatter,
-                      charge_formatter, radius_formatter, i_formatter];
+                      charge_formatter, radius_formatter, f_formatter, i_formatter];
 
     atoms.length = nodes[0].length;
     reset = reset || false;
