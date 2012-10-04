5247400ce1b26c2b1c6c4b56c207947fde48181c
Piotr Janik <janikpiotrek@gmail.com>
2012-10-04 13:15:41 +0200

    Refactor components test to use RequireJS
    [#36917391]

--- test/lab/components/components-test.js
+++ test/lab/components/components-test.js
@@ -1,23 +1,31 @@
 require("../../env");
 
-var vows = require("vows"),
-    assert = require("assert"),
+var requirejs = require('requirejs'),
+    config    = require('../../requirejs-config'),
+    vows = require("vows"),
+    assert = require("assert");
 
-    components = require("../../../server/public/lab/lab.components");
+// Use Lab RequireJS configuration.
+requirejs.config(config.labConfig);
 
-var suite = vows.describe("lab.components");
+requirejs([
+  'cs!components/thermometer'
+], function (Thermometer) {
 
-suite.addBatch({
-  "Thermometer": {
-    topic: function() {
-      return new components.Thermometer("#thermometer", 3.0, 0, 25);
-    },
-    "creates thermometer": function(therm) {
-      assert.equal(therm.max, 25);
-      assert.equal(therm.min, 0);
-      assert.equal(therm.value, 3);
+  var suite = vows.describe("lab.components");
+
+  suite.addBatch({
+    "Thermometer": {
+      topic: function() {
+        return new Thermometer("#thermometer", 3.0, 0, 25);
+      },
+      "creates thermometer": function(therm) {
+        assert.equal(therm.max, 25);
+        assert.equal(therm.min, 0);
+        assert.equal(therm.value, 3);
+      }
     }
-  }
-});
+  });
 
-suite.export(module);
+  suite.export(module);
+});
