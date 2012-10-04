881813220014a3f2d7b06015fe1aa236e854af47
Piotr Janik <janikpiotrek@gmail.com>
2012-10-04 13:14:45 +0200

    Add require-cs path to the Lab RequireJS config for unit tests
    [#36917391]

--- test/requirejs-config.js
+++ test/requirejs-config.js
@@ -1,5 +1,8 @@
 // Simple RequireJS config, common for Lab modules.
 exports.labConfig = {
   baseUrl: 'src/lab',
-  nodeRequire: require
+  nodeRequire: require,
+  paths: {
+    'cs' :'../vendor/require-cs/cs'
+  }
 };
