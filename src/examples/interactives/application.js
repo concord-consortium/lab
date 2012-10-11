a9d4f807410fd148afbf24ac07508437c19f8860
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-10-11 15:35:06 -0400

    disable Java MW link when no CML file available

--- src/examples/interactives/application.js
+++ src/examples/interactives/application.js
@@ -65,7 +65,7 @@ var ROOT = "/examples",
     if ($selectInteractive.length > 0) {
       selectInteractiveHandler();
     } else {
-      document.location.hash = '#interactives/oil-and-water-shake.json';
+      document.location.hash = '#interactives/basic-examples/oil-and-water-shake.json';
     }
   }
 
@@ -195,7 +195,8 @@ var ROOT = "/examples",
   }
 
   function setupFullPage() {
-    var java_mw_link;
+    var java_mw_href,
+        java_mw_link = $("#java-mw-link");
     $selectInteractive.val(interactiveUrl);
 
     // construct link to embeddable version of Interactive
@@ -209,9 +210,13 @@ var ROOT = "/examples",
     mmlPath = jsonModelPath.replace("/imports/legacy-mw-content/converted/", "").replace(".json", ".mml");
     contentItems = getObjects(modelList, "mml", mmlPath);
     if (contentItems.length > 0) {
-      java_mw_link = "/jnlp/jnlps/org/concord/modeler/mw.jnlp?version-id=1.0&jnlp-args=remote," +
+      java_mw_href = "/jnlp/jnlps/org/concord/modeler/mw.jnlp?version-id=1.0&jnlp-args=remote," +
                       window.location.origin + ACTUAL_ROOT + "/imports/legacy-mw-content/" + contentItems[0].cml;
-      $("#java-mw-link").attr("href", java_mw_link);
+      java_mw_link.attr("href", java_mw_href);
+    } else {
+      java_mw_link.removeAttr("href");
+      java_mw_link.attr("title", "Java MW link not available for this interactive");
+      java_mw_link.addClass("na");
     }
 
     // construct link to DataGames embeddable version of Interactive
