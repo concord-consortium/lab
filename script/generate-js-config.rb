b9d212c87ca4d7a0ff056432da9dce4d3086cf22
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-10-15 00:44:42 -0400

    lab.config.js: use :jsconfig section in config/config.yml
    [#37740577]

--- script/generate-js-config.rb
+++ script/generate-js-config.rb
@@ -1,15 +1,20 @@
 #!/usr/bin/env ruby
 require_relative 'setup.rb'
+require 'json'
 
 JS_CONFIG_PATH = File.join(SERVER_PUBLIC_PATH, 'lab', 'lab.config.js')
 
-config = <<HEREDOC
+# default jsconfig "sharing" is true
+if CONFIG[:jsconfig] == nil || CONFIG[:jsconfig][:sharing] == nil
+  CONFIG[:jsconfig] = { :sharing => true }
+end
+
+jsconfig = <<HEREDOC
 (function(){
-  if (typeof Lab === 'undefined') Lab = {};
-  Lab.config = {
-    "sharing": true
-  }
+if (typeof Lab === 'undefined') Lab = {};
+Lab.config = #{JSON.pretty_generate(CONFIG[:jsconfig])}
 })();
 HEREDOC
 
-File.open(JS_CONFIG_PATH, 'w') { |f| f.write config }
+File.open(JS_CONFIG_PATH, 'w') { |f| f.write jsconfig }
+
