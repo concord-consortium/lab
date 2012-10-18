9a38daf312c0ace171b608767c416c168131790b
Richard Klancer <rpk@pobox.com>
2012-10-18 15:39:08 -0400

    Remove 'window' reference from console stub. [#37911069]

--- src/lab/common/console.js
+++ src/lab/common/console.js
@@ -3,7 +3,11 @@ define(function (require) {
   // prevent a console.log from blowing things up if we are on a browser that
   // does not support it ... like IE9
   if (typeof console === 'undefined') {
-    window.console = {} ;
+    // Be sure to use 'self' to access the global object instead of 'window'. 'window' is not
+    // available in web workers, in which case 'self' provides access to the worker's global object.
+    // Note that 'self' is a venerable synonym for 'window' that is supported in all browsers, with
+    // or without worker support.
+    self.console = {};
     console.log = console.info = console.warn = console.error = function(){};
   }
 
