f5d9524e7c1c93c3d278a2d509f1c9a7a86499f3
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-10-26 16:46:58 -0400

    removed unused code from layout.js

--- src/lab/common/layout/layout.js
+++ src/lab/common/layout/layout.js
@@ -171,12 +171,6 @@ define(function (require) {
         }
         break;
 
-        // fluid (but normally the iframe doesn't expose the full-screen action)
-        case "simple-iframe":
-        setupSimpleFullScreenMoleculeContainer();
-        setupFullScreenDescriptionRight();
-        break;
-
         // fluid layout
         case "compare-screen":
         emsize = Math.min(layout.screen_factor_width * 1.1, layout.screen_factor_height);
@@ -207,29 +201,6 @@ define(function (require) {
       layout.checkbox_factor = Math.max(0.8, layout.checkbox_scale * layout.screen_factor);
       switch (layout.selection) {
 
-        // fluid layout
-        case "simple-screen":
-        emsize = Math.min(layout.screen_factor_width * 1.1, layout.screen_factor_height);
-        $('body').css('font-size', emsize + 'em');
-        simpleScreen();
-        break;
-
-        // only fluid on page load (and when resizing on trnasition to and from full-screen)
-        case "simple-static-screen":
-        if (layout.not_rendered) {
-          emsize = Math.min(layout.screen_factor_width * 1.1, layout.screen_factor_height);
-          $('body').css('font-size', emsize + 'em');
-          simpleStaticScreen();
-          layout.not_rendered = false;
-        }
-        break;
-
-        // fluid layout
-        case "simple-iframe":
-        emsize = Math.min(layout.screen_factor_width * 1.5, layout.screen_factor_height);
-        $('body').css('font-size', emsize + 'em');
-        setupSimpleIFrameScreen();
-        break;
 
         // only fluid on page load (and when resizing on trnasition to and from full-screen)
         case "full-static-screen":
@@ -248,7 +219,7 @@ define(function (require) {
         compareScreen();
         break;
 
-        // only fluid on page load (and when resizing on trnasition to and from full-screen)
+        // only fluid on page load (and when resizing on transition to and from full-screen)
         case "interactive":
         if (layout.not_rendered) {
           layout.setVizEmsize();
@@ -270,105 +241,8 @@ define(function (require) {
       }
       layout.regular_display = layout.getDisplayProperties();
     }
-    if (layout.transform) {
-      $('input[type=checkbox]').css(layout.transform, 'scale(' + layout.checkbox_factor + ',' + layout.checkbox_factor + ')');
-    }
-
-    if (layout.temperature_control_checkbox) {
-      model.addPropertiesListener(["temperature_control"], layout.temperatureControlUpdate);
-      layout.temperatureControlUpdate();
-    }
-
-    var benchmarks_table = document.getElementById("benchmarks-table");
-    if (benchmarks_table) {
-      benchmarks_table.style.display = "none";
-    }
 
     //
-    // Regular Screen Layout
-    //
-    function setupRegularScreen() {
-      var i, width, height, mcsize,
-          rightHeight, rightHalfWidth, rightQuarterWidth,
-          widthToPageRatio, modelAspectRatio,
-          pageWidth = layout.display.page.width,
-          pageHeight = layout.display.page.height;
-
-      mcsize = viewLists.moleculeContainers[0].scale();
-      modelAspectRatio = mcsize[0] / mcsize[1];
-      widthToPageRatio = mcsize[0] / pageWidth;
-      width = pageWidth * 0.40;
-      height = width * 1/modelAspectRatio;
-      if (height > pageHeight * 0.55) {
-        height = pageHeight * 0.55;
-        width = height * modelAspectRatio;
-      }
-      // HACK that will normally only work with one moleculeContainer
-      // or if all the moleculeContainers end up the same width
-      i = -1;  while(++i < viewLists.moleculeContainers.length) {
-        viewLists.moleculeContainers[i].resize(width, height);
-      }
-      rightQuarterWidth = (pageWidth - width) * 0.35;
-      rightHeight = height * 0.52;
-      i = -1;  while(++i < viewLists.potentialCharts.length) {
-        viewLists.potentialCharts[i].resize(rightQuarterWidth, rightHeight);
-      }
-      i = -1;  while(++i < viewLists.speedDistributionCharts.length) {
-        viewLists.speedDistributionCharts[i].resize(rightQuarterWidth, rightHeight);
-      }
-      rightHalfWidth = (pageWidth - width) * 0.72;
-      rightHeight = height * 0.76;
-      i = -1;  while(++i < viewLists.energyCharts.length) {
-        viewLists.energyCharts[i].resize(rightHalfWidth, rightHeight);
-      }
-    }
-
-    //
-    // Interactive Screen Layout
-    //
-    function setupInteractiveScreen() {
-      var i, width, height, mcsize,
-          modelWidth,
-          modelHeight,
-          modelDimensions,
-          modelAspectRatio,
-          modelWidthFactor,
-          viewSizes = {},
-          containerWidth = layout.viz.width,
-          containerHeight = layout.viz.height;
-
-      modelDimensions = viewLists.moleculeContainers[0].scale();
-      modelAspectRatio = modelDimensions[0] / modelDimensions[1];
-      modelWidthFactor = 0.70;
-      if (viewLists.thermometers) {
-        modelWidthFactor -= 0.10;
-      }
-      if (viewLists.energyGraphs) {
-        modelWidthFactor -= 0.45;
-      }
-      modelWidth = containerWidth * modelWidthFactor;
-      modelHeight = modelWidth / modelAspectRatio;
-      if (modelHeight > containerHeight * 0.60) {
-        modelHeight = containerHeight * 0.60;
-        modelWidth = modelHeight * modelAspectRatio;
-      }
-      viewSizes.moleculeContainers = [modelWidth, modelHeight];
-      if (viewLists.energyGraphs) {
-        viewSizes.energyGraphs = [containerWidth * 0.40, modelHeight * 1.25];
-      }
-      for (viewType in viewLists) {
-        if (viewLists.hasOwnProperty(viewType) && viewLists[viewType].length) {
-          i = -1;  while(++i < viewLists[viewType].length) {
-            if (viewSizes[viewType]) {
-              viewLists[viewType][i].resize(viewSizes[viewType][0], viewSizes[viewType][1]);
-            } else {
-              viewLists[viewType][i].resize();
-            }
-          }
-        }
-      }
-    }
-
     //
     // Interactive iframe Screen Layout
     //
@@ -436,8 +310,6 @@ define(function (require) {
       }
     }
 
-
-
     //
     // Compare Screen Layout
     //
@@ -500,68 +372,6 @@ define(function (require) {
     }
 
     //
-    // Simple Screen Layout
-    //
-    function simpleScreen() {
-      var i, width, height, mcsize, widthToPageRatio;
-
-      height = Math.min(layout.display.page.height * 0.45, layout.display.page.width * 0.50);
-      viewLists.moleculeContainers[0].resize(height, height);
-      mcsize = viewLists.moleculeContainers[0].scale();
-      widthToPageRatio = mcsize[0] / layout.display.page.width;
-      if (widthToPageRatio > 0.50) {
-        height *= (0.50 / widthToPageRatio);
-        viewLists.moleculeContainers[0].resize(height, height);
-      }
-      viewLists.thermometers[0].resize();
-    }
-
-    //
-    // Simple Static Screen Layout
-    //
-    function simpleStaticScreen() {
-      var i, width, height, mcsize, widthToPageRatio,
-          description_right = document.getElementById("description-right");
-
-      height = Math.min(layout.display.page.height * 0.65, layout.display.page.width * 0.50);
-      viewLists.moleculeContainers[0].resize(height, height);
-      mcsize = viewLists.moleculeContainers[0].scale();
-      widthToPageRatio = mcsize[0] / layout.display.page.width;
-      if (widthToPageRatio > 0.50) {
-        height *= (0.50 / widthToPageRatio);
-        viewLists.moleculeContainers[0].resize(height, height);
-        // if (description_right !== null) {
-        //   description_right.style.width = (layout.display.page.width - mcsize[0]) * 0.50 + "px";
-        // }
-      }
-      viewLists.thermometers[0].resize();
-    }
-
-    //
-    // Simple iframe Screen Layout
-    //
-    function setupSimpleIFrameScreen() {
-      var i, width, height, mcsize,
-          rightHeight, rightHalfWidth, rightQuarterWidth,
-          widthToPageRatio, modelAspectRatio,
-          pageWidth = layout.display.page.width,
-          pageHeight = layout.display.page.height;
-
-      mcsize = viewLists.moleculeContainers[0].scale();
-      modelAspectRatio = mcsize[0] / mcsize[1];
-      widthToPageRatio = mcsize[0] / pageWidth;
-      width = pageWidth * 0.70;
-      height = width * 1/modelAspectRatio;
-      if (height > pageHeight * 0.70) {
-        height = pageHeight * 0.70;
-        width = height * modelAspectRatio;
-      }
-      viewLists.moleculeContainers[0].resize(width, height);
-      mcsize = viewLists.moleculeContainers[0].scale();
-      viewLists.thermometers[0].resize();
-    }
-
-    //
     // Simple Full Screen Layout
     //
     function setupSimpleFullScreenMoleculeContainer() {
@@ -583,33 +393,6 @@ define(function (require) {
       viewLists.moleculeContainers[0].resize(width, height);
     }
 
-    function setupFullScreenDescriptionRight() {
-      var description_right = document.getElementById("description-right");
-      if (description_right !== null) {
-        // description_right.style.width = layout.display.window.width * 0.30 +"px";
-      }
-    }
-  };
-
-  layout.getStyleForSelector = function(selector) {
-    var rules, rule_lists = document.styleSheets;
-    for(var i = 0; i < rule_lists.length; i++) {
-      if (rule_lists[i]) {
-        try {
-           rules = rule_lists[i].rules || rule_lists[i].cssRules;
-           if (rules) {
-             for(var j = 0; j < rules.length; j++) {
-               if (rules[j].selectorText == selector) {
-                 return rules[j];
-               }
-             }
-           }
-        }
-        catch (e) {
-        }
-      }
-    }
-    return false;
   };
 
   // Adapted from getPageSize() by quirksmode.com
@@ -658,11 +441,6 @@ define(function (require) {
       }
       return false;
   };
-
-  var description_right = document.getElementById("description-right");
-  if (description_right !== null) {
-    layout.fontsize = parseInt(layout.getStyleForSelector("#description-right").style.fontSize);
-  }
 
   layout.transform = layout.getTransformProperty(document.body);
 
