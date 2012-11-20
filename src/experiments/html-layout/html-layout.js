30b5d3d97fa417fc6bc2a3c42fb18c2c37edc071
Piotr Janik <janikpiotrek@gmail.com>
2012-11-20 16:22:11 +0100

    Add algorithm for avoiding overflow in HTML Layout experiment

--- src/experiments/html-layout/html-layout.js
+++ src/experiments/html-layout/html-layout.js
@@ -1,8 +1,19 @@
 /*global Lab $ CodeMirror */
 
-function setupComponent(component, $modelContainer) {
+$.fn.hasScrollBar = function() {
+    //note: clientHeight= height of holder
+    //scrollHeight= we have content till this height
+    var _elm = $(this)[0];
+    var _hasScrollBar = false;
+   if ((_elm.clientWidth < _elm.scrollWidth) || (_elm.clientHeight < _elm.scrollHeight)) {
+        _hasScrollBar = true;
+    }
+    return _hasScrollBar;
+};
+
+function setupComponent(component, $modelContainer, updateOnly) {
   var $container = $('#' + component.id),
-      $el        = $('<div></div>'),
+      $el        = updateOnly ? $container.children() : $('<div></div>'),
 
       getDataWidth = function () {
         if ($container.data('width') === "model-width")
@@ -62,12 +73,14 @@ function setupComponent(component, $modelContainer) {
   $el.width("100%");
   $el.css('background', component.color);
 
-  $el.appendTo($container);
+  if (!updateOnly) {
+    $el.appendTo($container);
+  }
 
   return $container;
 }
 
-function setupLayout(layoutTpl, components) {
+function setupLayout(layoutTpl, components, updateOnly) {
   var i, modelComponent, $modelContainer;
 
   // Find model.
@@ -78,13 +91,20 @@ function setupLayout(layoutTpl, components) {
   }
 
   // Setup it, save reference.
-  $modelContainer = setupComponent(modelComponent);
+  $modelContainer = setupComponent(modelComponent, undefined, updateOnly);
 
   // Setup rest of the components.
   for (i = 0; i < components.length; i++) {
     if (!components[i].model) {
-      setupComponent(components[i], $modelContainer);
+      setupComponent(components[i], $modelContainer, updateOnly);
     }
+  }
+
+  if ($modelContainer.data('width') && $modelContainer.data('height') === undefined) {
+    return 'w';
+  }
+  else if($modelContainer.data('height') && $modelContainer.data('width') === undefined) {
+    return 'h';
   }
 }
 
@@ -112,15 +132,33 @@ $(function () {
 
   function update() {
     var newLayout     = layoutEditor.getValue(),
-        newComponents = interactiveEditor.getValue(),
-        $wrapper      = $('#wrapper');
+        newComponents = JSON.parse(interactiveEditor.getValue()),
+        $wrapper, $interCont, orientantion,
+        i = 0;
 
-    $wrapper.remove();
+    $('#wrapper').remove();
     $wrapper = $('<div id="wrapper"></div>').append(newLayout);
     $wrapper.css({width: "100%", height: "100%"});
 
     $('.view').append($wrapper);
-    setupLayout(newLayout, JSON.parse(newComponents));
+
+    orientantion = setupLayout(newLayout, newComponents, false);
+
+    if ($('#avoid-scrollbar').attr('checked') && orientantion !== '') {
+      $interCont = $('#interactive-container');
+      while($interCont.hasScrollBar() && i < 25) {
+        if (orientantion === 'w')
+          $interCont.width($interCont.width() * 0.95);
+        if (orientantion === 'h')
+          $interCont.height($interCont.height() * 0.95);
+        setupLayout(newLayout, newComponents, true);
+        i++;
+      }
+      if ($interCont.hasScrollBar()) {
+        $wrapper.remove();
+        $('.view').append($("<h2 id='wrapper'>Sorry, this layout can't handle current view size!</h2>"));
+      }
+    }
   }
 
   $(".view").resizable();
@@ -132,6 +170,11 @@ $(function () {
   // Select ready example.
   $("#preload").change(function () {
     document.location.hash = '#' + $("#preload").val();
+  });
+
+  // Select ready example.
+  $("#avoid-scrollbar").change(function () {
+    update();
   });
 
   // Update editor.
