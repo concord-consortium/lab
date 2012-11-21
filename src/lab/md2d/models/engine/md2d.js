concord-consortium/lab

commit 3efe77506adc0c16286319c3e728c516feeefbc3

Slightly refactor VDW Pairs related methods

src/lab/md2d/models/engine/md2d.js
@@ -31,8 +31,6 @@ define(function (require, exports, module) {
 
       BOLTZMANN_CONSTANT_IN_JOULES = constants.BOLTZMANN_CONSTANT.as( unit.JOULES_PER_KELVIN ),
 
-      DEFAULT_VALUES,
-
       ELEMENT_PROPERTY_LIST,
 
       ANGULAR_BOND_PROPERTY_LIST,
@@ -661,6 +659,16 @@ define(function (require, exports, module) {
           angularBonds.strength = arrays.create(num, 0, arrayTypes.float);
 
           assignShortcutReferences.angularBonds();
+        },
+
+        createVdwPairsArray = function() {
+          var maxNumPairs = N * (N-1) / 2;
+
+          vdwPairs = engine.vdwPairs = {};
+
+          vdwPairs.count = 0;
+          vdwPairs.atom1 = vdwPairAtom1Index = arrays.create(maxNumPairs, 0, arrayTypes.uint16);
+          vdwPairs.atom2 = vdwPairAtom2Index = arrays.create(maxNumPairs, 0, arrayTypes.uint16);
         },
 
         createSpringForcesArray = function(num) {
@@ -2140,19 +2148,7 @@ define(function (require, exports, module) {
         elementsHaveBeenCreated = true;
       },
 
-      createVdwPairsArray: function() {
-        var maxNumPairs = N * (N-1) / 2;
-
-        vdwPairs = engine.vdwPairs = {};
-
-        vdwPairs.count = N_vdwPairs;
-        vdwPairs.atom1 = vdwPairAtom1Index = arrays.create(maxNumPairs, 0, arrayTypes.uint16);
-        vdwPairs.atom2 = vdwPairAtom2Index = arrays.create(maxNumPairs, 0, arrayTypes.uint16);
-
-        engine.updateVdwPairsArray();
-      },
-
-      updateVdwPairsArray: function() {
+      getVdwPairsArray: function() {
         var i,
             j,
             dx,
@@ -2169,6 +2165,11 @@ define(function (require, exports, module) {
             sig,
             eps,
             distanceCutoff_sq = vdwLinesRatio * vdwLinesRatio;
+
+        // Lazy initialization of necessary structures during first function call.
+        if (vdwPairs === undefined) {
+          createVdwPairsArray();
+        }
 
         N_vdwPairs = 0;
 

src/lab/md2d/models/modeler.js
@@ -61,8 +61,6 @@ define(function(require) {
         angularBonds,
         // Restraints (currently atom-only)
         restraints,
-        // VDW Pairs
-        vdwPairs,
 
         // The index of the "spring force" used to implement dragging of atoms in a running model
         liveDragSpringForceIndex,
@@ -761,12 +759,6 @@ define(function(require) {
       return model;
     };
 
-    model.createVdwPairs = function(_atoms) {
-      engine.createVdwPairsArray(_atoms);
-      vdwPairs = engine.vdwPairs;
-      return model;
-    };
-
     model.createObstacles = function(_obstacles) {
       var numObstacles = _obstacles.x.length,
           i, prop, obstacleProps;
@@ -1266,8 +1258,7 @@ define(function(require) {
     };
 
     model.get_vdw_pairs = function() {
-      if (vdwPairs) engine.updateVdwPairsArray();
-      return vdwPairs;
+      return engine.getVdwPairsArray();
     };
 
     model.on = function(type, listener) {
@@ -1537,7 +1528,6 @@ define(function(require) {
     if (initialProperties.radialBonds)  model.createRadialBonds(initialProperties.radialBonds);
     if (initialProperties.angularBonds) model.createAngularBonds(initialProperties.angularBonds);
     if (initialProperties.restraints)   model.createRestraints(initialProperties.restraints);
-    if (initialProperties.showVDWLines) model.createVdwPairs(initialProperties.atoms);
     if (initialProperties.obstacles)    model.createObstacles(initialProperties.obstacles);
 
     // Initialize history if user provided atoms, etc, to save. Client code not passing in

commit 47770e46471a9574e6285303158a3a4c58ff1abb

Fix getting VDW array (broken in 3efe77506ad)

src/lab/md2d/models/engine/md2d.js
@@ -2226,7 +2226,6 @@ define(function (require, exports, module) {
         }
 
         vdwPairs.count = N_vdwPairs;
-
         return vdwPairs;
       },
 