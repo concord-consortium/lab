527dbc3f377f65ef1a1aa48ee08c550837d5e7d4
Piotr Janik <janikpiotrek@gmail.com>
2012-11-26 19:44:01 +0100

    Add 'count' parameter to serialize method
    It allows to limit number of serialized objects (useful, when array is bigger than real number of objects).

--- src/lab/common/serialize.js
+++ src/lab/common/serialize.js
@@ -4,13 +4,13 @@ define(function(require) {
 
   var arrays = require('arrays');
 
-  return function serialize(metaData, propertiesHash) {
+  return function serialize(metaData, propertiesHash, count) {
     var result = {}, propName, array;
     for (propName in metaData) {
       if (metaData.hasOwnProperty(propName)) {
         if (propertiesHash[propName] !== undefined && metaData[propName].serialize !== false) {
           array = propertiesHash[propName];
-          result[propName] = arrays.clone(array);
+          result[propName] = count !== undefined ? arrays.extend(array, count) : arrays.clone(array);
         }
       }
     }
