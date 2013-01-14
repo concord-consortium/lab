a9eaac6966ffa904655c82ae31fd8f9ee9f96d87
Piotr Janik <janikpiotrek@gmail.com>
2013-01-14 11:49:18 +0100

    Add validate method in DNA properties
    [#41019647]

--- src/lab/md2d/models/engine/dna-properties.js
+++ src/lab/md2d/models/engine/dna-properties.js
@@ -3,7 +3,9 @@
 define(function (require) {
 
   var validator = require('common/validator'),
-      metadata  = require('md2d/models/metadata');
+      metadata  = require('md2d/models/metadata'),
+
+      ValidationError = validator.ValidationError;
 
 
   return function DNAProperties() {
@@ -35,14 +37,14 @@ define(function (require) {
           data.complementarySequence = compSeq.toUpperCase();
         },
 
-        dnaValidation = function (props) {
+        customValidate = function (props) {
           if (props.sequence) {
             // Allow user to use both lower and upper case.
             props.sequence = props.sequence.toUpperCase();
 
             if (props.sequence.search(/[^AGTC]/) !== -1) {
               // Character other than A, G, T or C is found.
-              throw new Error("DNA code on sense strand can be defined using only A, G, T or C characters.");
+              throw new ValidationError("sequence", "DNA code on sense strand can be defined using only A, G, T or C characters.");
             }
           }
           return props;
@@ -53,7 +55,7 @@ define(function (require) {
 
           // Note that validator always returns a copy of the input object, so we can use it safely.
           props = validator.validateCompleteness(metadata.dnaProperties, props);
-          props = dnaValidation(props);
+          props = customValidate(props);
 
           // Note that validator always returns a copy of the input object, so we can use it safely.
           data = props;
@@ -70,7 +72,7 @@ define(function (require) {
 
           // Validate and update properties.
           props = validator.validate(metadata.dnaProperties, props);
-          props = dnaValidation(props);
+          props = customValidate(props);
 
           for (key in props) {
             if (props.hasOwnProperty(key)) {
@@ -109,6 +111,36 @@ define(function (require) {
       // Deserializes DNA properties.
       deserialize: function (props) {
         create(props);
+      },
+
+      // Convenient method for validation. It doesn't throw an exception,
+      // instead a special object with validation status is returned. It can
+      // be especially useful for UI classes to avoid try-catch sequences with
+      // "set". The returned status object always has a "valid" property,
+      // which contains result of the validation. When validation fails, also
+      // "errors" hash is provided which keeps error for property causing
+      // problems.
+      // e.g. {
+      //   valid: false,
+      //   errors: {
+      //     sequence: "DNA code on sense strand can be defined using only A, G, T or C characters."
+      //   }
+      // }
+      validate: function (props) {
+        var status = {
+          valid: true
+        };
+        try {
+          // Validation based on metamodel definition.
+          props = validator.validate(metadata.dnaProperties, props);
+          // Custom validation.
+          customValidate(props);
+        } catch (e) {
+          status.valid = false;
+          status.errors = {};
+          status.errors[e.prop] = e.message;
+        }
+        return status;
       },
 
       on: function(type, listener) {
