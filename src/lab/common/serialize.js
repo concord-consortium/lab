d1e01e9e207e471a1c45308560455630c5528ad8
Piotr Janik <janikpiotrek@gmail.com>
2013-01-23 15:31:32 -0500

    Support various properties types during serialization
    [#43089109]

--- src/lab/common/serialize.js
+++ src/lab/common/serialize.js
@@ -1,16 +1,24 @@
-/*global define: false */
+/*global define: false, $ */
 
 define(function(require) {
 
   var arrays = require('arrays');
 
   return function serialize(metaData, propertiesHash, count) {
-    var result = {}, propName, array;
+    var result = {}, propName, prop;
     for (propName in metaData) {
       if (metaData.hasOwnProperty(propName)) {
         if (propertiesHash[propName] !== undefined && metaData[propName].serialize !== false) {
-          array = propertiesHash[propName];
-          result[propName] = count !== undefined ? arrays.copy(arrays.extend(array, count), []) : arrays.copy(array, []);
+          prop = propertiesHash[propName];
+          if (arrays.isArray(prop)) {
+            result[propName] = count !== undefined ? arrays.copy(arrays.extend(prop, count), []) : arrays.copy(prop, []);
+          }
+          else if (typeof prop === 'object') {
+            result[propName] = $(true, {}, prop);
+          }
+          else {
+            result[propName] = prop;
+          }
         }
       }
     }
