74695830fa03ab6e6fbe4effc6aab6c1b73c0fd6
Noah Paessel <knowuh@gmail.com>
2013-01-24 19:02:49 -0500

    very minor formatting changes.

--- server/app/models/interactive.rb
+++ server/app/models/interactive.rb
@@ -1,9 +1,14 @@
 class Interactive < CouchRest::Model::Base
   # attr_accessor :casted_by
 
-  property :title,              String
-  property :publicationStatus,  String
+  property :title,               String
+  property :subtitle,            String
+  property :publicationStatus,   String
+  property :group_key,           String
+  property :path,                String
+  property :about,               String
 
+  # collections
   property :models,             [Object]
   property :components,         [Object]
 
