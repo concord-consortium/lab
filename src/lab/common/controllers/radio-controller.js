e0efdf3f1a49186a606a7891aaf7d0240df91c8f
Piotr Janik <janikpiotrek@gmail.com>
2013-01-28 16:41:15 -0500

    Implement serialization of the radio component
    [#43328491]

--- src/lab/common/controllers/radio-controller.js
+++ src/lab/common/controllers/radio-controller.js
@@ -28,8 +28,16 @@ define(function () {
         .append(option.text);
       $div.append($span).append("<br/>");
 
-      $option.change((function(option) {
+      $option.change((function(option, index) {
         return function() {
+          var i, len;
+
+          // Update component definition.
+          for (i = 0, len = options.length; i < len; i++) {
+            delete options[i].selected;
+          }
+          options[index].selected = true;
+
           if (option.action){
             scriptingAPI.makeFunctionInScriptContext(option.action)();
           } else if (option.loadModel){
@@ -37,7 +45,7 @@ define(function () {
             interactivesController.loadModel(option.loadModel);
           }
         };
-      })(option));
+      })(option, i));
     }
 
     // Public API.
@@ -50,6 +58,13 @@ define(function () {
       // Returns view container.
       getViewContainer: function () {
         return $div;
+      },
+
+      // Returns serialized component definition.
+      serialize: function () {
+        // Return compoment definition. It's always valid,
+        // as selected option is updated during 'change' callback.
+        return $.extend(true, {}, component);
       }
     };
     // Return Public API object.
