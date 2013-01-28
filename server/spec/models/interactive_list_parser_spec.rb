d0eb30225954e80351ebee5123f9b85885300939
Noah Paessel <knowuh@gmail.com>
2013-01-28 10:34:50 -0500

    List Parsing: Mock details of interactive parsing to isolate tests.

--- server/spec/models/interactive_list_parser_spec.rb
+++ server/spec/models/interactive_list_parser_spec.rb
@@ -18,7 +18,13 @@ describe InteractiveListParser do
   end
 
   describe "#parse" do
-    subject { InteractiveListParser.new(json_uri).parse }
+    subject do 
+      parser = InteractiveListParser.new(json_uri)
+      interactive_parser_class = mock(:new => mock(:parse => nil))
+      parser.interactive_parser = interactive_parser_class
+      parser.parse
+    end
+    
     context "a simple list with valid interactive definitions in it" do
       # as of Jan 2013 ~95 interactives
       its(:interactives) { should have_at_least(90).items } 
