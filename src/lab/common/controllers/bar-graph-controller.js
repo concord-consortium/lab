0ca6eb3cf7de3087d49c3a669f01679c072f90e7
Piotr Janik <janikpiotrek@gmail.com>
2013-01-29 13:27:25 -0500

    Implement serialization of the bar graph component
    [#43328491]

--- src/lab/common/controllers/bar-graph-controller.js
+++ src/lab/common/controllers/bar-graph-controller.js
@@ -1,17 +1,65 @@
-/*global define: false, model: false */
+/*global $: false, define: false, model: false */
 
 // Bar graph controller.
 // It provides specific interface used in MD2D environment
 // (by interactives-controller and layout module).
 define(function (require) {
   var BarGraphModel = require('grapher/bar-graph/bar-graph-model'),
-      BarGraphView  = require('grapher/bar-graph/bar-graph-view');
+      BarGraphView  = require('grapher/bar-graph/bar-graph-view'),
+
+      // Note: We always explicitly copy properties from component spec to bar graph options hash,
+      // in order to avoid tighly coupling an externally-exposed API (the component spec) to an
+      // internal implementation detail (the bar graph options format).
+      barGraphOptionForComponentSpecProperty = {
+        // Min value displayed.
+        minValue:  'minValue',
+        // Max value displayed.
+        maxValue:  'maxValue',
+        // Graph title.
+        title:     'title',
+        // Color of the main bar.
+        barColor:  'barColor',
+        // Color of the area behind the bar.
+        fillColor: 'fillColor',
+        // Color of axis, labels, title.
+        textColor: 'textColor',
+        // Number of ticks displayed on the axis.
+        // This value is *only* a suggestion. The most clean
+        // and human-readable values are used.
+        ticks:      'ticks',
+        // Number of subdivisions between major ticks.
+        tickSubdivide: 'tickSubdivide',
+        // Enables or disables displaying of numerical labels.
+        displayLabels: 'displayLabels',
+        // Format of labels.
+        // See the specification of this format:
+        // https://github.com/mbostock/d3/wiki/Formatting#wiki-d3_format
+        // or:
+        // http://docs.python.org/release/3.1.3/library/string.html#formatspec
+        labelFormat: 'labelFormat'
+      },
+
+      // Limit options only to these supported.
+      filterOptions = function(inputHash) {
+        var options = {},
+            cName, gName;
+
+        for (cName in barGraphOptionForComponentSpecProperty) {
+          if (barGraphOptionForComponentSpecProperty.hasOwnProperty(cName)) {
+            gName = barGraphOptionForComponentSpecProperty[cName];
+            if (inputHash[cName] !== undefined) {
+              options[gName] = inputHash[cName];
+            }
+          }
+        }
+        return options;
+      };
 
   return function BarGraphController(component) {
     var // Object with Public API.
         controller,
         // Model with options and current value.
-        barGraphModel = new BarGraphModel(component.options),
+        barGraphModel = new BarGraphModel(filterOptions(component.options)),
         // Main view.
         barGraphView  = new BarGraphView({model: barGraphModel, id: component.id}),
         // First data channel.
@@ -50,6 +98,15 @@ define(function (require) {
           width: width,
           height: height
         });
+      },
+
+      // Returns serialized component definition.
+      serialize: function () {
+        var result = $.extend(true, {}, component);
+        // Update options.
+        result.options = filterOptions(barGraphModel.toJSON());
+        // Return updated definition.
+        return result;
       }
     };
 
