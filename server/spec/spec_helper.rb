3df2e84c94590abe6821c5c8c8f34c9baed761e7
Noah Paessel <knowuh@gmail.com>
2013-01-29 15:59:54 -0500

    Don't disable access to localhost in spec_helper. (broke CouchDB calls)

--- server/spec/spec_helper.rb
+++ server/spec/spec_helper.rb
@@ -5,6 +5,10 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'webmock/rspec'
 
+
+# by default, all web connections are disabled.
+WebMock.disable_net_connect!(:allow_localhost => true)
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
