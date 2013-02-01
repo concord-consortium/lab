eba307ec7d7c222c2c08daeaf397ee3502e49791
Noah Paessel <knowuh@gmail.com>
2013-02-01 10:18:39 -0500

    integration/parsing_spec md2d should include viewOptions fem Interactive
    Also the url has changed.

--- server/spec/integration/parsing_spec.rb
+++ server/spec/integration/parsing_spec.rb
@@ -7,21 +7,30 @@ describe "Round Trip parsing" do
     let(:interactive_path){ sample_file_path interactive_file }
     let(:model_file) { "models/md2d/one-atom.json"}
     let(:interactive_hash){ JSON.parse(read_sample_file interactive_file) }
-    let(:model_hash)      { JSON.parse(read_sample_file model_file) }
+    let(:model_raw_hash)  { JSON.parse(read_sample_file model_file) }
 
+    # Merge in the viewOptions for each model spec
+    let(:view_options) do 
+      interactive_hash['models'][0]['viewOptions']
+    end
+    let(:model_hash)      do 
+      (model_raw_hash['viewOptions'] = view_options) if view_options
+      model_raw_hash
+    end
+  
     it "should have matching interactive hash" do
       interactive = Parsers::Interactive.new(interactive_path).parse()
       presenter   = Presenters::Interactive.new(interactive)
       interactive_hash['models'].each do |m|
-        m['url'].sub!("models/md2d","http://localhost:3000/md2d_models")
+        m['url'].sub!("models/md2d","http://localhost:3000/models/md2ds")
       end
       presenter.runtime_properties.should == interactive_hash
     end
 
     it "should have matchign models hashes" do
       interactive = Parsers::Interactive.new(interactive_path).parse()
-      interactive.md_2d_models.each do |m|
-        presenter   = Presenters::Md2dModel.new(m)
+      interactive.md2ds.each do |m|
+        presenter   = Presenters::Models::Md2d.new(m)
         presenter.runtime_properties.should == model_hash
       end
     end
