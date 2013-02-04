a9bd9a59f5e13a760c2b548dbacdecdb7feea1bd
Noah Paessel <knowuh@gmail.com>
2013-02-04 16:49:26 -0500

    parsing_spec : deep assertions about Interactive's data-hash

--- server/spec/integration/parsing_spec.rb
+++ server/spec/integration/parsing_spec.rb
@@ -3,28 +3,67 @@ require 'spec_helper'
 describe "Round Trip parsing" do
 
   describe "parsing a known interactive" do
+
     let(:interactive_file){ "interactives/basic-examples/one-atom.json" }
-    let(:interactive_path){ sample_file_path interactive_file }
-    let(:model_file) { "models/md2d/one-atom.json"}
-    let(:interactive_hash){ JSON.parse(read_sample_file interactive_file) }
-    let(:model_raw_hash)  { JSON.parse(read_sample_file model_file) }
+    let(:interactive_path){ sample_file_path interactive_file      }
+    let(:model_file)      { "models/md2d/one-atom.json"            }
+    let(:model_raw_hash)  { JSON.parse(read_sample_file model_file)}
+
+    let(:interactive_hash) do
+      JSON.parse(read_sample_file interactive_file)
+    end
 
     # Merge in the viewOptions for each model spec
-    let(:view_options) do 
+    let(:view_options) do
       interactive_hash['models'][0]['viewOptions']
     end
-    let(:model_hash)      do 
+
+    let(:model_hash)      do
       (model_raw_hash['viewOptions'] = view_options) if view_options
       model_raw_hash
     end
-  
+
     it "should have matching interactive hash" do
       interactive = Parsers::Interactive.new(interactive_path).parse()
       presenter   = Presenters::Interactive.new(interactive)
-      interactive_hash['models'].each do |m|
-        m['url'].sub!("models/md2d","http://localhost:3000/models/md2ds")
-      end
-      presenter.runtime_properties.should == interactive_hash
+      props = presenter.runtime_properties
+
+      props.should include({
+        "about" => "",
+        "publicationStatus" => "public",
+        "subtitle" => "The MD2D model works with just a single atom also.",
+        "title" => "Testing: Only One Atom"
+      })
+
+      props['models'].should include({
+        "id" => "one-atom",
+        "type" => "md2d",
+        "url" => "http://localhost:3000/models/md2ds/models_md2d_one-atom.json",
+        "viewOptions" => {
+          "controlButtons" => "play_reset",
+          "enableAtomTooltips" => true
+        }
+      })
+
+      props['components'].should include({
+        "displayValue" => "return format('f')(value)",
+        "id" => "current-obstacles",
+        "property" => "temperature",
+        "type" => "numericOutput"
+      })
+
+      props['components'].should include({
+        "action" => [
+          "var atom = getAtomProperties(0);",
+          "atom.vx += (Math.random()*2-1)*1e-4;",
+          "atom.vy += (Math.random()*2-1)*1e-4;",
+          "setAtomProperties(0, { vx: atom.vx, vy: atom.vy });"
+        ],
+        "id" => "kick-atom",
+        "text" => "Kick Atom",
+        "type" => "button"
+      })
+
     end
 
     it "should have matchign models hashes" do
