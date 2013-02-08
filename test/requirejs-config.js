71b88886962afd3bc119399a3affc584e15b4b04
Piotr Janik <janikpiotrek@gmail.com>
2013-02-08 03:32:59 +0100

    Update RequireJS config for tests
    [#43328491]
    
    Support modules which use backbone.

--- test/requirejs-config.js
+++ test/requirejs-config.js
@@ -7,6 +7,24 @@ exports.labConfig = {
     // Define RequireJS plugin for CoffeScript loading.
     'cs': '../vendor/require-cs/cs',
       // Plugin for loading plain text files (GLSL sources).
-    'text': '../vendor/text/text'
+    'text': '../vendor/text/text',
+    'underscore': '../vendor/underscore/underscore',
+    'backbone': '../vendor/backbone/backbone'
+  },
+
+  // The shim config allows us to configure dependencies for
+  // scripts that do not call define() to register a module
+  shim: {
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      deps: [
+        'underscore'
+        //'jquery' is also required, but the whole 'Lab' libary depends on jQuery,
+        // which is expected to be available on the host website.
+      ],
+      exports: 'Backbone'
+    }
   }
 };
