84fa90ba88ab888d4586a0e0872753a1a15f8fe9
Piotr Janik <janikpiotrek@gmail.com>
2013-02-12 16:24:24 +0100

    Change slightly styling of components and layout template
    [#44281603] [#44382415]

--- src/lab/common/layout/templates.js
+++ src/lab/common/layout/templates.js
@@ -1,91 +1,88 @@
-define(function (require) {
-
-  templates = {
-		"simple": [
-		  {
-		    "id": "top",
-		    "bottom": "model.top"
-		  },
-		  {
-		    "id": "right",
-		    "left": "model.right",
-		    "height": "model.height",
-		    "padding-left": "1em"
-		  },
-		  {
-		    "id": "bottom",
-		    "top": "model.bottom",
-		    "width": "model.width + right.width",
-		    "padding-top": "1em"
-		  }
-		],
-		"narrow-right": [
-			{
-		    "id": "top",
-		    "bottom": "model.top"
-		  },
-			{
-		    "id": "right",
-		    "left": "model.right",
-		    "height": "model.height",
-		    "padding-left": "1em",
-		    "width": "6em"
-		  },
-		  {
-		    "id": "bottom",
-		    "top": "model.bottom",
-		    "width": "model.width + right.width",
-		    "padding-top": "1em"
-		  }
-		],
-		"split-right": [
-			{
-		    "id": "top",
-		    "bottom": "model.top"
-		  },
-			{
-		    "id": "right-top",
-		    "left": "model.right",
-		    "height": "model.height/2",
-		    "padding-left": "1em"
-		  },
-			{
-		    "id": "right-bottom",
-		    "left": "model.right",
-		    "top": "model.top + model.height/2",
-		    "height": "model.height/2",
-		    "padding-left": "1em"
-		  },
-		  {
-		    "id": "bottom",
-		    "top": "model.bottom",
-		    "width": "model.width + right-top.width",
-		    "padding-top": "1em"
-		  }
-		],
-		"big-top": [
-			{
-		    "id": "top",
-		    "bottom": "model.top",
-		    "height": "model.height/3",
-		    "width": "model.width + right.width",
-		    "padding-bottom": "1em"
-		  },
-			{
-		    "id": "right",
-		    "left": "model.right",
-		    "height": "model.height",
-		    "padding-left": "1em"
-		  },
-		  {
-		    "id": "bottom",
-		    "top": "model.bottom",
-		    "width": "model.width + right.width",
-		    "padding-top": "1em"
-		  }
-		]
-	};
-
-	return templates;
-
+/*global define*/
+define(function () {
+  return {
+    "simple": [
+      {
+        "id": "top",
+        "bottom": "model.top"
+      },
+      {
+        "id": "right",
+        "left": "model.right",
+        "height": "model.height",
+        "padding-left": "1em"
+      },
+      {
+        "id": "bottom",
+        "top": "model.bottom",
+        "width": "model.width + right.width",
+        "padding-bottom": "1em"
+      }
+    ],
+    "narrow-right": [
+      {
+        "id": "top",
+        "bottom": "model.top"
+      },
+      {
+        "id": "right",
+        "left": "model.right",
+        "height": "model.height",
+        "padding-left": "1em",
+        "width": "6em"
+      },
+      {
+        "id": "bottom",
+        "top": "model.bottom",
+        "width": "model.width + right.width",
+        "padding-top": "1em"
+      }
+    ],
+    "split-right": [
+      {
+        "id": "top",
+        "bottom": "model.top"
+      },
+      {
+        "id": "right-top",
+        "left": "model.right",
+        "height": "model.height/2",
+        "padding-left": "1em"
+      },
+      {
+        "id": "right-bottom",
+        "left": "model.right",
+        "top": "model.top + model.height/2",
+        "height": "model.height/2",
+        "padding-left": "1em"
+      },
+      {
+        "id": "bottom",
+        "top": "model.bottom",
+        "width": "model.width + right-top.width",
+        "padding-top": "1em"
+      }
+    ],
+    "big-top": [
+      {
+        "id": "top",
+        "bottom": "model.top",
+        "height": "model.height/3",
+        "width": "model.width + right.width",
+        "padding-bottom": "1em"
+      },
+      {
+        "id": "right",
+        "left": "model.right",
+        "height": "model.height",
+        "padding-left": "1em"
+      },
+      {
+        "id": "bottom",
+        "top": "model.bottom",
+        "width": "model.width + right.width",
+        "padding-top": "1em"
+      }
+    ]
+  };
 });
