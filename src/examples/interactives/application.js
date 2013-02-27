ec33552d9f50722d7b96e48e72c57f7a2807a7af
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-02-26 22:03:02 -0500

    add missing space char
    [#45204215]

--- src/examples/interactives/application.js
+++ src/examples/interactives/application.js
@@ -257,7 +257,7 @@ AUTHORING = false;
     if (!Lab.config.sharing) {
       
       $creditsContent.append('<p>Find a <a href=' + interactiveCreditsUrl +
-        ' class="opens-in-new-window" target="_blank">shareable version</a> of this interactivealong with dozens of other open-source interactives for science, math and engineering at <a href="' +
+        ' class="opens-in-new-window" target="_blank">shareable version</a> of this interactive along with dozens of other open-source interactives for science, math and engineering at <a href="' +
         concordUrl + '" class="opens-in-new-window" target="_blank">concord.org</a>.</p>');
     }
     $creditsContent.append(copyrightDiv);
