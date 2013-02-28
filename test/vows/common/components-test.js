0ab41b576ad4e10acaae1a39f5177564e4e8be66
Piotr Janik <janikpiotrek@gmail.com>
2013-02-28 18:21:55 +0100

    Update Energy2D tests because of the RequireJS 2.1 new specification
    [#45324607]
    
    Change requirejs call to sync version.
    Use sync form:
      var dep = require('dep');
    instead of async form:
      requirejs(['dep'], function(dep) {
        (...)
      });

--- test/vows/common/components-test.js
+++ test/vows/common/components-test.js
@@ -1,31 +1,25 @@
-require("../../env");
+var helpers   = require("../../helpers");
+helpers.setupBrowserEnvironment();
 
-var requirejs = require('requirejs'),
-    config    = require('../../requirejs-config'),
-    vows = require("vows"),
-    assert = require("assert");
+var vows      = require("vows"),
+    assert    = require("assert"),
+    requirejs = helpers.getRequireJS(),
 
-// Use Lab RequireJS configuration.
-requirejs.config(config.labConfig);
+    Thermometer = requirejs('cs!common/components/thermometer'),
 
-requirejs([
-  'cs!common/components/thermometer'
-], function (Thermometer) {
+    suite = vows.describe("common/components");
 
-  var suite = vows.describe("common/components");
-
-  suite.addBatch({
-    "Thermometer": {
-      topic: function() {
-        return new Thermometer("#thermometer", 3.0, 0, 25);
-      },
-      "creates thermometer": function(therm) {
-        assert.equal(therm.max, 25);
-        assert.equal(therm.min, 0);
-        assert.equal(therm.value, 3);
-      }
+suite.addBatch({
+  "Thermometer": {
+    topic: function() {
+      return new Thermometer("#thermometer", 3.0, 0, 25);
+    },
+    "creates thermometer": function(therm) {
+      assert.equal(therm.max, 25);
+      assert.equal(therm.min, 0);
+      assert.equal(therm.value, 3);
     }
-  });
-
-  suite.export(module);
+  }
 });
+
+suite.export(module);
--- test/vows/energy2d/part-test.js
+++ test/vows/energy2d/part-test.js
@@ -1,116 +1,106 @@
-/*jslint indent: 2, sloppy: true, node: true */
+var helpers   = require("../../helpers");
+helpers.setupBrowserEnvironment();
 
-require("../../env");
+var vows      = require("vows"),
+    assert    = require("assert"),
+    requirejs = helpers.getRequireJS(),
 
-var
-  requirejs = require('requirejs'),
-  config    = require('../../requirejs-config'),
-  vows      = require("vows"),
-  assert    = require("assert");
-
-// Use Lab RequireJS configuration.
-requirejs.config(config.labConfig);
-
-requirejs([
-  'energy2d/models/core-model',
-  'energy2d/models/part',
-  'energy2d/models/shape'
-], function (core, part, shape) {
-  var
+    core      = requirejs('energy2d/models/core-model'),
+    part      = requirejs('energy2d/models/part'),
+    shape     = requirejs('energy2d/models/shape'),
     Part      = part.Part,
     Rectangle = shape.Rectangle,
 
-    suite     = vows.describe("energy2d/models/part");
+    suite = vows.describe("energy2d/models/part");
 
-  suite.addBatch({
-    'Part': {
-      'initialized with empty options (no shape defined)': {
-        'should throw an exception': function () {
-          assert.throws(function () { var test = new part.Part({}); }, Error);
-        }
-      },
-      'initialized with (dummy) options': {
-        topic: function () {
-          var
-            options = {
-              rectangle: {
-                x: 1, y: 2, width: 3, height: 4
-              },
-              thermal_conductivity: 5,
-              specific_heat: 6,
-              density: 7,
-              temperature: 8,
-              constant_temperature: 9,
-              power: 10,
-              wind_speed: 11,
-              wind_angle: 12,
-
-              transmission: 13,
-              reflection: 14,
-              absorption: 15,
-              emissivity: 16,
-
-              visible: 17,
-              filled: 18,
-              color: 19,
-              texture: 20,
-              label: 21
+suite.addBatch({
+  'Part': {
+    'initialized with empty options (no shape defined)': {
+      'should throw an exception': function () {
+        assert.throws(function () { var test = new part.Part({}); }, Error);
+      }
+    },
+    'initialized with (dummy) options': {
+      topic: function () {
+        var
+          options = {
+            rectangle: {
+              x: 1, y: 2, width: 3, height: 4
             },
-            part = new Part(options);
+            thermal_conductivity: 5,
+            specific_heat: 6,
+            density: 7,
+            temperature: 8,
+            constant_temperature: 9,
+            power: 10,
+            wind_speed: 11,
+            wind_angle: 12,
 
-          return { part: part, options: options };
-        },
-        'should have properties set': function (pair) {
-          assert.deepEqual(pair.part.rectangle, pair.options.rectangle);
-          assert.instanceOf(pair.part.shape, Rectangle);
-          assert.equal(pair.part.thermal_conductivity, pair.options.thermal_conductivity);
-          assert.equal(pair.part.specific_heat, pair.options.specific_heat);
-          assert.equal(pair.part.density, pair.options.density);
-          assert.equal(pair.part.temperature, pair.options.temperature);
-          assert.equal(pair.part.constant_temperature, pair.options.constant_temperature);
-          assert.equal(pair.part.power, pair.options.power);
-          assert.equal(pair.part.wind_speed, pair.options.wind_speed);
-          assert.equal(pair.part.wind_angle, pair.options.wind_angle);
+            transmission: 13,
+            reflection: 14,
+            absorption: 15,
+            emissivity: 16,
 
-          assert.equal(pair.part.transmission, pair.options.transmission);
-          assert.equal(pair.part.reflection, pair.options.reflection);
-          assert.equal(pair.part.absorption, pair.options.absorption);
-          assert.equal(pair.part.emissivity, pair.options.emissivity);
+            visible: 17,
+            filled: 18,
+            color: 19,
+            texture: 20,
+            label: 21
+          },
+          part = new Part(options);
 
-          assert.equal(pair.part.visible, pair.options.visible);
-          assert.equal(pair.part.filled, pair.options.filled);
-          assert.equal(pair.part.color, pair.options.color);
-          assert.equal(pair.part.texture, pair.options.texture);
-          assert.equal(pair.part.label, pair.options.label);
-        }
+        return { part: part, options: options };
       },
-      'created in context of the simulation model, which is emmisive': {
-        topic: function () {
-          var options = {
-            structure: {
-              part: [
-                {
-                  rectangle: {
-                    x: 4, y: 4, width: 2, height: 2
-                  },
-                  emissivity: 1,
-                  temperature: 50 // greater than MINIMAL_RADIATING_TEMPERATURE = 20
-                }
-              ]
-            }
-          };
-          return new core.makeCoreModel(options);
-        },
-        'should radiate': function (model) {
-          var part = model.getPartsArray()[0];
+      'should have properties set': function (pair) {
+        assert.deepEqual(pair.part.rectangle, pair.options.rectangle);
+        assert.instanceOf(pair.part.shape, Rectangle);
+        assert.equal(pair.part.thermal_conductivity, pair.options.thermal_conductivity);
+        assert.equal(pair.part.specific_heat, pair.options.specific_heat);
+        assert.equal(pair.part.density, pair.options.density);
+        assert.equal(pair.part.temperature, pair.options.temperature);
+        assert.equal(pair.part.constant_temperature, pair.options.constant_temperature);
+        assert.equal(pair.part.power, pair.options.power);
+        assert.equal(pair.part.wind_speed, pair.options.wind_speed);
+        assert.equal(pair.part.wind_angle, pair.options.wind_angle);
 
-          assert.lengthOf(model.getPhotonsArray(), 0);
-          assert.equal(model.getTemperatureAt(5, 4), 50);
-          part.radiate(model);
-          assert.isTrue(model.getPhotonsArray().length > 0);
-        }
+        assert.equal(pair.part.transmission, pair.options.transmission);
+        assert.equal(pair.part.reflection, pair.options.reflection);
+        assert.equal(pair.part.absorption, pair.options.absorption);
+        assert.equal(pair.part.emissivity, pair.options.emissivity);
+
+        assert.equal(pair.part.visible, pair.options.visible);
+        assert.equal(pair.part.filled, pair.options.filled);
+        assert.equal(pair.part.color, pair.options.color);
+        assert.equal(pair.part.texture, pair.options.texture);
+        assert.equal(pair.part.label, pair.options.label);
+      }
+    },
+    'created in context of the simulation model, which is emmisive': {
+      topic: function () {
+        var options = {
+          structure: {
+            part: [
+              {
+                rectangle: {
+                  x: 4, y: 4, width: 2, height: 2
+                },
+                emissivity: 1,
+                temperature: 50 // greater than MINIMAL_RADIATING_TEMPERATURE = 20
+              }
+            ]
+          }
+        };
+        return new core.makeCoreModel(options);
+      },
+      'should radiate': function (model) {
+        var part = model.getPartsArray()[0];
+
+        assert.lengthOf(model.getPhotonsArray(), 0);
+        assert.equal(model.getTemperatureAt(5, 4), 50);
+        part.radiate(model);
+        assert.isTrue(model.getPhotonsArray().length > 0);
       }
     }
-  });
-  suite.export(module);
+  }
 });
+suite.export(module);
