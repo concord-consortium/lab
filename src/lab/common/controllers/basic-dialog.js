068b5815705a188be80e1e719353cc0cbcc69859
Piotr Janik <janikpiotrek@gmail.com>
2013-03-01 13:04:41 +0100

    Add Basic Class dialog, simple wrapper around the jQuery UI Dialog
    [#44817219]
    
    It provides some useful defaults and convenient API.

---
+++ src/lab/common/controllers/basic-dialog.js
@@ -0,0 +1,37 @@
+/*global define, $ */
+/**
+  Simple wrapper around the jQuery UI Dialog providing
+  some useful defaults and simple interface.
+*/
+define(function () {
+
+  var defOptions = {
+    autoOpen: false,
+    dialogClass: "interactive-dialog",
+    // Ensure that font is being scaled dynamically!
+    appendTo: "#responsive-content",
+    width: "30em"
+  };
+
+  function BasicDialog(options) {
+    // Basic dialog elements.
+    this.$element = $('<div>');
+    // jQuery UI Dialog.
+    this.$element.dialog($.extend({}, defOptions, options));
+  }
+
+  BasicDialog.prototype.open = function() {
+    this.$element.dialog("open");
+  };
+
+  BasicDialog.prototype.set = function(key, value) {
+    this.$element.dialog("option", key, value);
+  };
+
+  BasicDialog.prototype.setContent = function ($content) {
+    this.$element.empty();
+    this.$element.append($content);
+  };
+
+  return BasicDialog;
+});
