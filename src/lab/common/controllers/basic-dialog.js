3bd191e39bdbe80a7ac8280dd2d348bd3709a524
Piotr Janik <janikpiotrek@gmail.com>
2013-03-05 10:17:58 +0100

    Disable autofocus in dialogs
    [#44817219]
    
    Also improve docs of the BasicDialog class.

--- src/lab/common/controllers/basic-dialog.js
+++ src/lab/common/controllers/basic-dialog.js
@@ -1,8 +1,5 @@
 /*global define, $ */
-/**
-  Simple wrapper around the jQuery UI Dialog providing
-  some useful defaults and simple interface.
-*/
+
 define(function () {
 
   var defOptions = {
@@ -13,23 +10,51 @@ define(function () {
     width: "80%"
   };
 
+  /**
+   * Simple wrapper around the jQuery UI Dialog,
+   * which provides useful defaults and simple interface.
+   *
+   * @constructor
+   * @param {Object} options jQuery UI Dialog options.
+   */
   function BasicDialog(options) {
-    // Basic dialog elements.
+    /**
+     * Basic dialog elements.
+     * @type {jQuery}
+     */
     this.$element = $('<div>');
-    // jQuery UI Dialog.
+    // Create jQuery UI Dialog.
     this.$element.dialog($.extend({}, defOptions, options));
   }
 
+  /**
+   * Opens the dialog.
+   */
   BasicDialog.prototype.open = function() {
     this.$element.dialog("open");
   };
 
+  /**
+   * Sets jQuery UI Dialog option.
+   *
+   * @param {string} key
+   * @param {Object} value
+   */
   BasicDialog.prototype.set = function(key, value) {
     this.$element.dialog("option", key, value);
   };
 
+  /**
+   * Sets content of the dialog.
+   *
+   * @param {jQuery|DOM|string} $content Any value that can be accepted by the jQuery constructor.
+   */
   BasicDialog.prototype.setContent = function ($content) {
     this.$element.empty();
+    // Not very pretty, but probably the simplest and most reliable way to
+    // disable autofocus in jQuery UI dialogs. See:
+    // http://jqueryui.com/upgrade-guide/1.10/#added-ability-to-specify-which-element-to-focus-on-open
+    this.$element.append('<input type="hidden" autofocus="autofocus" />');
     this.$element.append($content);
   };
 
