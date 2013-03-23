326600c3a03eb1f850267331c84c7c6b8bb62618
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-03-23 10:29:23 -0400

    gh-pages generated from 5c6d50ead2ecf64a9c72dc257530edb4021fac36

--- lab-amd/lab.version.js
+++ lab-amd/lab.version.js
@@ -4,14 +4,14 @@ define(function (require) {
     "repo": {
       "branch": "master",
       "commit": {
-        "sha":           "71d0e13b62d6e93b82bbd742d92b1f4f502f5057",
-        "short_sha":      "71d0e13b",
-        "url":            "https://github.com/concord-consortium/lab/commit/71d0e13b",
+        "sha":           "5c6d50ead2ecf64a9c72dc257530edb4021fac36",
+        "short_sha":      "5c6d50ea",
+        "url":            "https://github.com/concord-consortium/lab/commit/5c6d50ea",
         "author":        "Stephen Bannasch",
         "email":         "stephen.bannasch@gmail.com",
-        "date":          "2013-03-23 09:51:59 -0400",
-        "short_message": "server/public/fonts/Font-Awesome: remove .git folder",
-        "message":       "server/public/fonts/Font-Awesome: remove .git folder\n\n[#46761159]\n\nWhen copying resources to server/public make sure that\nthe .git modulars that deify the original folder as a\ngit submodule do not end up in server/public.\n\nNot only does it add unnecessarily to the static distribution size\nthe presence of the .git folder prevents the gh-pages.rb script\nfrom completing when it tries to add the new files to the gh-pages\nbranch:\n\n$ git add .\nfatal: Not a git repository: vendor/fonts/Font-Awesome/../../../../.git/modules/src/vendor/fonts/Font-Awesome\n\nproblem introduced in: 643b854d"
+        "date":          "2013-03-23 10:24:18 -0400",
+        "short_message": "index.html: use relative url to fonts",
+        "message":       "index.html: use relative url to fonts\n\n[#46761483]\n\nAlso fix Guardfile to correctly generate index.css\nusing updated Sass invocation used in Makefile."
       },
       "dirty": false
     }
--- vendor/jquery/jquery.js
+++ vendor/jquery/jquery.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://jquery.org/license
  *
- * Date: Sat Mar 23 2013 09:57:13 GMT-0400 (EDT)
+ * Date: Sat Mar 23 2013 10:25:38 GMT-0400 (EDT)
  */
 (function( window, undefined ) {
 var
