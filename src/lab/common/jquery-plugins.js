c6f84c1da86d48f55ad5cf55bf15f5f02b0ebe74
Piotr Janik <janikpiotrek@gmail.com>
2013-04-11 12:44:06 +0200

    Implement .truncate() function as jQuery plugin
    [#44616153]
    
    It truncates text inside given element, so its width doesn't exceed
    specified value (in pixels). Note that you *can* use this function
    even on elements like <p> or <h1>, which quite often have width of its
    parent (not width of their text). This function will create a new
    <span> element with the same style as original text and use it to
    measure real width of the text.

--- src/lab/common/jquery-plugins.js
+++ src/lab/common/jquery-plugins.js
@@ -28,4 +28,45 @@ define(function () {
     el.remove();
     return result;
   };
+
+  /**
+   * Truncates text inside given element, so its width doesn't exceed specified
+   * value (in pixels). Note that you *can* use this function even on elements
+   * like <p> or <h1>, which quite often have width of its parent (not width of
+   * their text). This function will create a new <span> element with the same
+   * style as original text and use it to measure real width of the text.
+   *
+   * @param  {number} maxWidth Maximum allowed width of text.
+   */
+  $.fn.truncate = function (maxWidth) {
+    var $el = $(this),
+        $span = $('<span>'),
+        width,
+        newText;
+
+    $span.text($el.text());
+    $span.css({
+      'font-size': $el.css('font-size'),
+      'font-weight': $el.css('font-weight'),
+      'white-space': 'nowrap',
+      'visibility': 'hidden'
+    });
+    $span.appendTo($el.parent());
+
+    width = $span.width();
+
+    if (width > maxWidth) {
+      newText = $span.text() + "...";
+      $span.text(newText);
+      while (width > maxWidth && newText.length > 3) {
+        newText = $span.text().slice(0, -4) + "...";
+        $span.text(newText);
+        width = $span.width();
+      }
+      // Update original text.
+      $el.text(newText);
+    }
+    // Cleanup!
+    $span.remove();
+  };
 });
