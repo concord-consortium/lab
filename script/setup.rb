f83eebc0c85c522d4b19fd4f0f68a8319bee9618
Tom Dyer <tdyer@ourstage.com>
2013-04-11 11:56:21 -0400

    Track GA pageviews after the interactive is loaded with ajax

--- script/setup.rb
+++ script/setup.rb
@@ -29,16 +29,9 @@ end
 if CONFIG[:google_analytics] && CONFIG[:google_analytics][:account_id]
   ANALYTICS = <<-HEREDOC
   <script type="text/javascript">
-    // make an array out of the URL's hashtag string, splitting the string at every ampersand
-    var my_hashtag_array = location.hash.split('&');
-
-    // grab the first value of the array (assuming that's the value that indicates which interactive is being viewed)
-    var my_hashtag = my_hashtag_array[0];
-
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', '#{CONFIG[:google_analytics][:account_id]}']);
     _gaq.push(['_setAllowAnchor', true]);
-    _gaq.push(['_trackPageview', location.pathname + my_hashtag]);
     (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
