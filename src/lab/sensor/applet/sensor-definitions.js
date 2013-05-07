1a028f24c19fba4d3a597b9eb47271ee90fd7bfe
Richard Klancer <rpk@pobox.com>
2013-05-07 13:10:39 -0400

    sensor definitions file
    [#48362817]

---
+++ src/lab/sensor/applet/sensor-definitions.js
@@ -0,0 +1,12 @@
+/*global define: false*/
+
+define(function() {
+  return {
+    goMotion: {
+      appletClass: 'goio',
+      sensorType: 'distance',
+      samplesPerSecond: 20,
+      tareable: true
+    }
+  };
+});
