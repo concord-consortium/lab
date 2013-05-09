53e8126c399ec386e361595793ca6ddc8bd39e08
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-05-09 11:42:31 -0400

    also rewrite url to interactives.json
    [#49515835]
    
    Now referencing this link:
    
      http://lab.dev.concord.org/examples/interactives/interactives.json
    
    Will redirect to where the actual JSON content is available:
    
      http://lab.dev.concord.org/interactives.json

--- server/config/initializers/rack_rewrite.rb
+++ server/config/initializers/rack_rewrite.rb
@@ -1,4 +1,5 @@
 App::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
   r301   '/examples/interactives/interactives.html',  '/interactives.html'
-  r301   '/examples/interactives/embeddable.html',  '/embeddable.html'
+  r301   '/examples/interactives/embeddable.html',    '/embeddable.html'
+  r301   '/examples/interactives/interactives.json',  '/interactives.json'
 end
