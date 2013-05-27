60a9f1f482575df22a54b3dccc70f7efea0d39e8
Piotr Janik <janikpiotrek@gmail.com>
2013-05-27 15:26:49 +0200

    Support multiple objects per name in StateManager
    [#50263057]

--- src/lab/common/views/state-manager.js
+++ src/lab/common/views/state-manager.js
@@ -9,27 +9,27 @@ define(function () {
     api = {
       newState: function (stateName, stateDef) {
         var state = $.extend(true, {}, stateDef);
-        names.forEach(function (v) {
-          if (typeof state[v] === "undefined") {
-            state[v] = [];
+        names.forEach(function (n) {
+          if (typeof state[n] === "undefined") {
+            state[n] = [];
           }
         });
         states.push(state);
         stateByName[stateName] = state;
       },
       extendLastState: function (stateName, stateDef) {
-        var state = $.extend(true, {}, states[states.length - 1], stateDef),
-            objName;
-        for (objName in state) {
-          if (state.hasOwnProperty(objName)) {
-            // Set all object that are undefined (or == null) in the new state
-            // to [] (empty data set). So to keep previous object data, just
-            // define it, but do not update anything.
-            if (typeof stateDef[objName] === "undefined" || stateDef[objName] === null) {
-              state[objName] = [];
-            }
+        var prevState = states[states.length - 1],
+            state = {};
+
+        names.forEach(function (n) {
+          state[n] = [];
+          if (typeof stateDef[n] !== "undefined") {
+            // Array expected!
+            stateDef[n].forEach(function (objDef, idx) {
+              state[n].push($.extend(true, {}, prevState[n][idx], objDef));
+            });
           }
-        }
+        });
         states.push(state);
         stateByName[stateName] = state;
       },
--- test/mocha/common/state-manager.coffee
+++ test/mocha/common/state-manager.coffee
@@ -28,16 +28,20 @@ describe "StateManager", ->
       sm.getState("2").should.eql {atom: [{x: 2}], trees: [{color: "blue"}, {color: "black"}], rocks: []}
 
     it "should allow extending the last state", ->
-      # Note that trees are untouched. They are defined to make sure that they are not deleted.
-      # However, they are not updated!
-      state = {atom: [{x: 3}], trees: []}
+      # Note that trees are untouched. They are defined, but no option is actually updated.
+      state = {atom: [{x: 3}], trees: [{}, {}]}
       sm.extendLastState "3", state
       sm.getState("3").should.eql {atom: [{x: 3}], trees: [{color: "blue"}, {color: "black"}], rocks: []}
+
+      # Remove one tree, add new property to atom.
+      state = {atom: [{y: 4}], trees: [{}]}
+      sm.extendLastState "3.5", state
+      sm.getState("3.5").should.eql {atom: [{x: 3, y: 4}], trees: [{color: "blue"}], rocks: []}
 
       # There is no definition of trees anymore, so it will be deleted!
       state = {atom: [{x: 4}]}
       sm.extendLastState "4", state
-      sm.getState("4").should.eql {atom: [{x: 4}], trees: [], rocks: []}
+      sm.getState("4").should.eql {atom: [{x: 4, y: 4}], trees: [], rocks: []}
 
     it "should evaluate all functions in state during getState() operation", ->
       test = 2
