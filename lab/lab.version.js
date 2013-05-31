03cfafee3cfebf2d9cda055aa624098e7096a009
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-05-30 22:46:48 -0400

    generated from commit: a42054f355a8a7fcd07066687564288a33bf9cfb
    Stephen Bannasch <stephen.bannasch@gmail.com>
    Thu May 30 22:40:06 2013 -0400
    
        handle missing Java MW modelList
    
        Java MW modelList.js is not included in gh-pages-based
    distribution of public/ dir

---
+++ lab/lab.version.js
@@ -0,0 +1,19 @@
+// this file is generated during build process by: ./script/generate-js-version.rb
+define(function (require) {
+  return {
+    "repo": {
+      "branch": "master",
+      "commit": {
+        "sha":           "fb6cb3afb088de661438676a1db718248af8f21b",
+        "short_sha":      "fb6cb3af",
+        "url":            "https://github.com/concord-consortium/lab/commit/fb6cb3af",
+        "author":        "Stephen Bannasch",
+        "email":         "stephen.bannasch@gmail.com",
+        "date":          "2013-05-30 22:20:10 -0400",
+        "short_message": "use main commit for gh-pages commit message",
+        "message":       "use main commit for gh-pages commit message"
+      },
+      "dirty": false
+    }
+  };
+});
