836940654f8519be3cc03591bba649b2dedc50de
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-05-30 21:36:56 -0400

    script/archive-public.sh generates public/ tarball

---
+++ script/archive-public.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+if [ ! -d public/.git ]
+then
+cat <<heredoc
+
+*** dir: public not yet setup as a separate gh-pages tracking branch
+
+    Please setup the public as a separate git checkout tracking the gh-pages branch first.
+    See: http://lab.dev.concord.org/readme.html#making-the-public-folder-track-the-gh-pages-branch
+
+heredoc
+exit
+fi
+if ps aux | grep -v grep | grep 'ruby bin/guard' > /dev/null
+then
+cat <<heredoc
+
+*** shut down bin/guard first before generating an archive of the gh-pages
+    branch in public/
+
+heredoc
+exit
+else
+
+if git diff --exit-code --quiet && git diff --cached --exit-code --quiet
+then
+archivename = concord-consortium-lab-`git --git-dir ../.git log -1 --format=%h`.tar.gz
+mkdir -p tarballs
+cat <<heredoc
+
+*** generating an archive of the gh-page branch in public/
+    into ./tarballs/$archivename
+
+heredoc
+cd public
+git archive HEAD | gzip > ../tarballs/$archivename
+cd ..
+else
+cat <<heredoc
+
+*** uncommitted changes in your working directory
+
+    please commit or stash changes in your working dir before generating
+    an archive of the gh-pages branch in public/
+
+heredoc
+git status
+fi
+fi
