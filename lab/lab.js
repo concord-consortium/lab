012177b0b647cf9a3a4fdb89e028db4f75d3accb
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-05-30 22:32:05 -0400

    generated from commit: fb6cb3afb088de661438676a1db718248af8f21b
    Stephen Bannasch <stephen.bannasch@gmail.com>
    Thu May 30 22:20:10 2013 -0400
    
        use main commit for gh-pages commit message

--- lab/lab.js
+++ lab/lab.js
@@ -413,14 +413,14 @@ define('lab.version',['require'],function (require) {
     "repo": {
       "branch": "master",
       "commit": {
-        "sha":           "25a3beba5dbe1c8debeffe736a99a05559805ef2",
-        "short_sha":      "25a3beba",
-        "url":            "https://github.com/concord-consortium/lab/commit/25a3beba",
+        "sha":           "fb6cb3afb088de661438676a1db718248af8f21b",
+        "short_sha":      "fb6cb3af",
+        "url":            "https://github.com/concord-consortium/lab/commit/fb6cb3af",
         "author":        "Stephen Bannasch",
         "email":         "stephen.bannasch@gmail.com",
-        "date":          "2013-05-30 21:59:00 -0400",
-        "short_message": "fix script/archive-public.sh",
-        "message":       "fix script/archive-public.sh"
+        "date":          "2013-05-30 22:20:10 -0400",
+        "short_message": "use main commit for gh-pages commit message",
+        "message":       "use main commit for gh-pages commit message"
       },
       "dirty": false
     }
--- vendor/jquery/jquery.js
+++ vendor/jquery/jquery.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://jquery.org/license
  *
- * Date: 2013-05-31T02:04Z
+ * Date: 2013-05-31T02:25Z
  */
 (function( window, undefined ) {
 
