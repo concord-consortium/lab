fca923ea3f3d2d414c09d624d735dbfcdf99d7ff
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-06-12 13:40:25 -0400

    cap: new tasks: checkout, bundle_install
    checkout:
    checkout branch and force reset to origin/branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,37 +35,44 @@ end
 
 namespace :deploy do
 
-  desc "setup server"
-  task :setup do
-    run "cd /var/www/app; git fetch"
+  desc "restart the Rack application"
+  task :restart do
+    run "touch /var/www/app/tmp/restart.txt"
+  end
+
+  desc "checkout branch and force reset to origin/branch"
+  task :checkout do
+    run "cd /var/www/app; git fetch --all"
+    run "cd /var/www/app; git checkout #{branch}"
     run "cd /var/www/app; git reset --hard origin/#{branch}"
-    run "cd /var/www/app; bundle install"
-    run "cd /var/www/app; cp config/config.sample.yml config/config.yml"
-    run "cd /var/www/app; make clean; make"
-    update_jnlps
+  end
+
+  desc "update Ruby Gems with bundle install"
+  task :bundle_install do
+    run "cd /var/www/app; bundle install --binstubs"
   end
 
   desc "update server"
   task :update do
-    run "cd /var/www/app; git checkout #{branch}; git pull origin #{branch}"
-    run "cd /var/www/app; bundle install"
-    run "cd /var/www/app; make clean-public"
-    run "cd /var/www/app; make"
+    checkout
+    bundle_install
+    run "cd /var/www/app; make clean-public; make"
+    restart
   end
-
 
   desc "clean and update server"
   task :clean_and_update do
-    run "cd /var/www/app; git fetch"
-    run "cd /var/www/app; git reset --hard origin/#{branch}"
-    run "cd /var/www/app; bundle install"
+    checkout
+    bundle_install
     run "cd /var/www/app; make clean; make"
+    restart
   end
 
   desc "clean and rebuild jars in public/jnlp dir on server"
   task :update_jnlps do
-    run "cd /var/www/app; git checkout #{branch}; git pull origin #{branch}"
+    checkout
     run "cd /var/www/app; make jnlp-all"
+    restart
   end
 
   desc "clean and update server and recreate jar resources"
@@ -74,14 +81,17 @@ namespace :deploy do
     update_jnlps
   end
 
+  desc "setup server"
+  task :setup do
+    checkout
+    run "cd /var/www/app; bundle install --binstubs"
+    run "cd /var/www/app; cp config/config.sample.yml config/config.yml"
+    clean_and_update_all
+  end
+
   desc "display last commit on deployed server"
   task :status do
     run "cd /var/www/app; git log -1"
-  end
-
-  desc "restart the Rack application"
-  task :restart do
-    run "touch /var/www/app/tmp/restart.txt"
   end
 
   desc "create symbolic link from old urls to root dir"
