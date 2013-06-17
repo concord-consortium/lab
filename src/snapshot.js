3b9f6ebcb6e90f98ea06798892c075037186a849
Noah Paessel <knowuh@gmail.com>
2013-06-17 14:34:09 -0400

    snapshot.js: phantomjs : Canvases ➙ data-url png's

--- src/snapshot.js
+++ src/snapshot.js
@@ -1,3 +1,4 @@
+/*global $ */
 (function(){
   var getBaseUrl = function() {
     var base = window.location.protocol +  "//" + window.location.host + "/";
@@ -11,7 +12,30 @@
     var height  = element.height();
     var html_content;
 
+    var replacementImgs = element.find('canvas').map( function(count,elem) {
+        var dataUrl = elem.toDataURL('image/png');
+        var img = $("<img>");
+        var width = $(elem).width();
+        var height = $(elem).height();
+
+        img.attr('src', dataUrl);
+
+        img.attr("style", $(elem).attr("style"));
+        img.addClass($(elem).attr("class"));
+
+        img.attr('width', width);
+        img.attr('height', height);
+
+        img.width(width);
+        img.height(height);
+        return img;
+    });
+
     element = element.clone();
+    element.find('canvas').each(function(i,elm) {
+      $(elm).replaceWith(replacementImgs[i]);
+    });
+
     element.css({
       'top':0,
       'left':0,
