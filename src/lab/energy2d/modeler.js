e7700b786cf9f3a5aad5741370074a7d57d91dcf
Piotr Janik <janikpiotrek@gmail.com>
2013-06-19 16:09:51 +0200

    Implement Energy2D modeler consistent with rest of the framework
    [#51873425]

--- src/lab/energy2d/modeler.js
+++ src/lab/energy2d/modeler.js
@@ -1,42 +1,145 @@
-/*jslint indent: 2, browser: true, newcap: true */
-/*globals define: false*/
+/*global define: false, d3: false */
 
-// TODO: Remove this, as it's redundant.
 define(function (require) {
   'use strict';
-  var coremodel = require('energy2d/models/core-model');
+  var metadata        = require('energy2d/metadata'),
+      validator       = require('common/validator'),
+      coremodel       = require('energy2d/models/core-model'),
+      LabModelerMixin = require('common/lab-modeler-mixin'),
 
-  return function Modeler(options) {
-    var core_model = coremodel.makeCoreModel(options);
+      unitsDefinition = {
+        units: {
+          time: {
+            name: "second",
+            pluralName: "seconds",
+            symbol: "s"
+          }
+        }
+      };
 
-    return {
+  return function Modeler(initialProperties) {
+    var model,
+        core_model,
+
+        interval_id,
+
+        dispatch = d3.dispatch("tick", "play", "stop", "reset", "stepForward", "stepBack"),
+
+        labModelerMixin = new LabModelerMixin({
+          metadata: metadata,
+          unitsDefinition: unitsDefinition
+        }),
+        propertySupport = labModelerMixin.propertySupport;
+
+    model = {
+      // API required by the Lab framework:
+      resetTime: function () {},
+      on: function (type, listener) {
+        dispatch.on(type, listener);
+      },
+      // Required by playback controller.
+      is_stopped: function () {
+        return !interval_id;
+      },
+
+      start: function () {
+        if (!interval_id) {
+          interval_id = setInterval(model.nextStep, 0);
+          dispatch.play();
+        }
+      },
+
+      stop: function () {
+        if (interval_id !== undefined) {
+          clearInterval(interval_id);
+          interval_id = undefined;
+          dispatch.stop();
+        }
+      },
+
+      // Custom API:
       nextStep: function () {
         core_model.nextStep();
-      },
-      getWidth: function () {
-        return core_model.getModelOptions().model_width;
-      },
-      getHeight: function () {
-        return core_model.getModelOptions().model_height;
+        model.updateAllOutputProperties();
+        dispatch.tick();
       },
       getTime: function () {
-        return core_model.getModelOptions().timestep * core_model.getIndexOfStep();
+        return model.properties.timestep * core_model.getIndexOfStep();
       },
-      isWebGLActive: core_model.isWebGLActive,
-      getWebGLError: core_model.getWebGLError,
-      getIndexOfStep: core_model.getIndexOfStep,
-      getGridWidth: core_model.getGridWidth,
-      getGridHeight: core_model.getGridHeight,
-      getTemperatureArray: core_model.getTemperatureArray,
-      getTemperatureTexture: core_model.getTemperatureTexture,
-      getUVelocityArray: core_model.getUVelocityArray,
-      getVVelocityArray: core_model.getVVelocityArray,
-      getVelocityTexture: core_model.getVelocityTexture,
-      getPhotonsArray: core_model.getPhotonsArray,
-      getPartsArray: core_model.getPartsArray,
-      updateTemperatureArray: core_model.updateTemperatureArray,
-      updateVelocityArrays: core_model.updateVelocityArrays,
-      setPerformanceTools: core_model.setPerformanceTools
+
+      isWebGLActive: function () {
+        return core_model.isWebGLActive();
+      },
+      getWebGLError: function () {
+        return core_model.getWebGLError();
+      },
+      getIndexOfStep: function () {
+        return core_model.getIndexOfStep();
+      },
+      getTemperatureArray: function () {
+        return core_model.getTemperatureArray();
+      },
+      getTemperatureTexture: function () {
+        return core_model.getTemperatureTexture();
+      },
+      getUVelocityArray: function () {
+        return core_model.getUVelocityArray();
+      },
+      getVVelocityArray: function () {
+        return core_model.getVVelocityArray();
+      },
+      getVelocityTexture: function () {
+        return core_model.getVelocityTexture();
+      },
+      getPhotonsArray: function () {
+        return core_model.getPhotonsArray();
+      },
+      getPartsArray: function () {
+        return core_model.getPartsArray();
+      },
+      updateTemperatureArray: function () {
+        return core_model.updateTemperatureArray();
+      },
+      updateVelocityArrays: function () {
+        return core_model.updateVelocityArrays();
+      },
+      setPerformanceTools: function () {
+        return core_model.setPerformanceTools();
+      }
     };
+
+    (function () {
+      var viewOptions,
+          mainProperties;
+
+      // Adds model.properties, model.set, model.get, model.addObserver, model.removeObserver...
+      labModelerMixin.mixInto(model);
+
+      mainProperties = validator.validateCompleteness(metadata.mainProperties, initialProperties);
+      propertySupport.setRawValues(mainProperties);
+
+      viewOptions = validator.validateCompleteness(metadata.viewOptions, initialProperties.viewOptions || {});
+      propertySupport.setRawValues(viewOptions);
+
+      core_model = coremodel.makeCoreModel(model.properties);
+
+      model.defineOutput('time', {
+        label: "Time",
+        unitType: 'time',
+        format: '.2f'
+      }, function() {
+        return model.getTime();
+      });
+
+      model.defineOutput('displayTime', {
+        label: "Time",
+        unitType: 'time',
+        format: '.2f'
+      }, function() {
+        return model.getTime();
+      });
+    }());
+
+    return model;
   };
 });
