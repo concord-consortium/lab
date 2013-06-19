a9500167ca69c88edae8e8929298a27f9d8539c7
Piotr Janik <janikpiotrek@gmail.com>
2013-06-19 16:11:06 +0200

    Implement Energy2D model container
    [#51873425]

---
+++ src/lab/energy2d/views/model-container.js
@@ -0,0 +1,74 @@
+/* global define, $ */
+
+define(function() {
+  var VisualizationContainer = require('energy2d/views/visualization-container'),
+      use_WebGL = false;
+
+  return function View() {
+    var api,
+        model,
+        energy2d_scene,
+        heatmap_view,
+        velocity_view,
+        parts_view,
+        photons_view;
+
+    function createEnergy2DScene () {
+      energy2d_scene = new VisualizationContainer('model-container', use_WebGL);
+      heatmap_view = energy2d_scene.getHeatmapView();
+      velocity_view = energy2d_scene.getVelocityView();
+      photons_view = energy2d_scene.getPhotonsView();
+      parts_view = energy2d_scene.getPartsView();
+
+      return energy2d_scene;
+    }
+
+    api = {
+      $el: null,
+
+      getHeightForWidth: function(width) {
+        return width * model.properties.grid_height / model.properties.grid_width;
+      },
+
+      resize: function() {
+        api.update();
+        parts_view.renderParts();
+      },
+
+      reset: function() {},
+
+      update: function () {
+        heatmap_view.renderHeatmap();
+        velocity_view.renderVectormap();
+        photons_view.renderPhotons();
+      },
+
+      bindModel: function(newModel) {
+        model = newModel;
+        model.on('tick.view-update', api.update);
+
+        var props = model.properties;
+
+        energy2d_scene.setVisualizationOptions(props);
+        parts_view.bindPartsArray(model.getPartsArray(), props.model_width, props.model_height);
+        photons_view.bindPhotonsArray(model.getPhotonsArray(), props.model_width, props.model_height);
+
+        if (use_WebGL) {
+          heatmap_view.bindHeatmapTexture(model.getTemperatureTexture());
+          velocity_view.bindVectormapTexture(model.getVelocityTexture(), props.grid_width, props.grid_height, 25);
+        } else {
+          heatmap_view.bindHeatmap(model.getTemperatureArray(), props.grid_width, props.grid_height);
+          velocity_view.bindVectormap(model.getUVelocityArray(), model.getVVelocityArray(), props.grid_width, props.grid_height, 25);
+        }
+
+        api.update();
+        parts_view.renderParts();
+      }
+    };
+
+    energy2d_scene = createEnergy2DScene();
+    api.$el = energy2d_scene.getHTMLElement();
+
+    return api;
+  };
+});
