1e16aba17970cd860e8cf96ba248d9dfbe10f099
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-07-03 17:47:35 -0400

    use lowercase hyphenated model-type keys
    [#50749305]
    
    Use the same form of lowercase hyphenated model-type
    keys as used in the framework.

--- src/helpers/lab.json.js
+++ src/helpers/lab.json.js
@@ -6,11 +6,11 @@ requirejs.config({baseUrl: "src/lab"});
 var labMetaData = {
       interactive: requirejs('common/controllers/interactive-metadata'),
       models: {
-        md2d:            requirejs('md2d/models/metadata'),
-        energy2d:        requirejs('energy2d/metadata'),
-        sensor:          requirejs('sensor/metadata'),
-        signalGenerator: requirejs('signal-generator/metadata'),
-        solarSystem:     requirejs('solar-system/models/metadata')
+        "md2d":             requirejs('md2d/models/metadata'),
+        "energy2d":         requirejs('energy2d/metadata'),
+        "sensor":           requirejs('sensor/metadata'),
+        "signal-generator": requirejs('signal-generator/metadata'),
+        "solar-system":     requirejs('solar-system/models/metadata')
       }
     };
 
