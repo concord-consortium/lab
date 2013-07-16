92c7dd2009deb9af80e22c748e703e70f7b85973
Piotr Janik <janikpiotrek@gmail.com>
2013-07-16 12:42:35 +0200

    Add new module for function connected with colors manipulation
    [#48204939]

---
+++ src/lab/common/views/color.js
@@ -0,0 +1,36 @@
+/*global define: false, d3: false */
+
+/**
+ * Tiny module providing useful functions for color manipulation.
+ */
+define(function () {
+
+  function parseColor(color) {
+    // d3.rgb is handy, however it cannor parse RGBA colors. Use it regexp to
+    // parse rgba if it's necessary. Note that alpha channel will be ignored!
+    var rgba = color.match(/rgba\(([0-9]+),([0-9]+),([0-9]+),([0-9]+)\)/i);
+    if (rgba !== null) {
+      return d3.rgb(rgba[1], rgba[2], rgba[3]);
+    } else {
+      return d3.rgb(color);
+    }
+  }
+
+  return {
+    /**
+     * Returns color contrasting to specified background color (black or white).
+     * Note that if background color specifies alpha channel (e.g. rgba(0,0,0,0.5)),
+     * it will be ignored!
+     * @param  {string} bg Web-compatible color definition (e.g. "red", "#ff0012", "#000").
+     * @return {string} Contrasting color - "#000" or "#fff".
+     */
+    contrastingColor: function (bg) {
+      bg = parseColor(bg);
+      // Calculate luminance in YIQ color space.
+      // This ensures that color will be visible on background.
+      // This simple algorithm is described here:
+      // http://www.w3.org/TR/AERT#color-contrast
+      return (bg.r * 299 + bg.g * 587 + bg.b * 114) / 1000 >= 128 ? '#000' : '#fff';
+    }
+  };
+});
