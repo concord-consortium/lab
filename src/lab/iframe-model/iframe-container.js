5c7d6faddb756b82510bda5fc32888332fc5ed81
Scott Cytacki <scytacki@concord.org>
2013-08-07 22:17:38 -0400

    update iframe-model aspectRatio if width/height change
    [#53655105]

--- src/lab/iframe-model/iframe-container.js
+++ src/lab/iframe-model/iframe-container.js
@@ -1,7 +1,8 @@
 /*global $, define: false */
 define(function() {
   return function(model, modelUrl) {
-    var $el = $("<div id='model-container' class='container'>" +
+    var _model = model,
+        $el = $("<div id='model-container' class='container'>" +
                "<iframe id='iframe-model' " +
                  "style='width:100%;height:100%' src='" + model.get('url') + "'>" +
                "</iframe>" +
@@ -10,12 +11,13 @@ define(function() {
     return  {
       $el: $el,
       getHeightForWidth: function(width) {
-        var aspectRatio = model.get('width')/model.get('height');
+        var aspectRatio = _model.get('width')/_model.get('height');
         return width/aspectRatio;
       },
       resize: function() {},
       bindModel: function(newModel, newModelUrl) {
         $el.find('#iframe-model').attr('src', newModel.get('url'));
+        _model = newModel;
       },
       setup: function() {},
       update: function() {}
