88c4e5db9d4146c34f01102f32f729b9d422054c
Scott Cytacki <scytacki@concord.org>
2013-08-07 22:04:20 -0400

    make sure iframe updates when url is changed
    [#53655105]

--- src/lab/iframe-model/iframe-container.js
+++ src/lab/iframe-model/iframe-container.js
@@ -1,16 +1,22 @@
 /*global $, define: false */
 define(function() {
   return function(model, modelUrl) {
+    var $el = $("<div id='model-container' class='container'>" +
+               "<iframe id='iframe-model' " +
+                 "style='width:100%;height:100%' src='" + model.get('url') + "'>" +
+               "</iframe>" +
+             "</div>");
+
     return  {
-      $el: $("<div id='model-container' class='container'>" +
-             "<iframe style='width:100%;height:100%' src='" + model.get('url') + "'></iframe>" +
-             "</div>"),
+      $el: $el,
       getHeightForWidth: function(width) {
         var aspectRatio = model.get('width')/model.get('height');
         return width/aspectRatio;
       },
       resize: function() {},
-      bindModel: function() {},
+      bindModel: function(newModel, newModelUrl) {
+        $el.find('#iframe-model').attr('src', newModel.get('url'));
+      },
       setup: function() {},
       update: function() {}
     };
