ecc0b1f6ab83d00d7b97e2a1228e165de952d3b7
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-08-13 13:45:42 -0400

    also re-generate lab.min.js for static distribution

--- script/create-archived-public-dir.sh
+++ script/create-archived-public-dir.sh
@@ -58,6 +58,7 @@ cd ..
 LAB_DISABLE_SHUTTERBUG=true LAB_DISABLE_MODEL_LIST=true  bin/haml -r ./script/setup.rb src/interactives.html.haml public/interactives.html
 LAB_DISABLE_SHUTTERBUG=true LAB_DISABLE_MODEL_LIST=true  bin/haml -r ./script/setup.rb src/embeddable.html.haml public/embeddable.html
 rm -f src/lab/lab.config.js; make public/lab/lab.js STATIC=true
+make public/lab/lab.min.js
 cd public
 git commit -am "commit html files modified for static distribution into static branch"
 git checkout public
