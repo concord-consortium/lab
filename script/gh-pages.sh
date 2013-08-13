0e509c2779a9f29d93bf2d2f1fb8c87ef3b7fa05
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-08-13 19:00:46 -0400

    gh-pages: restore non-static html pages when done

--- script/gh-pages.sh
+++ script/gh-pages.sh
@@ -38,6 +38,8 @@ git add --all .
 git commit -am "generated from commit: `git --git-dir ../.git log -1 --format="%H%n%n%an <%ae>%n%cd%n%n    %s%n%n    %b"`"
 git push origin gh-pages
 cd ..
+bin/haml -r ./script/setup.rb src/interactives.html.haml public/interactives.html
+bin/haml -r ./script/setup.rb src/embeddable.html.haml public/embeddable.html
 rm -f src/lab/lab.config.js; make public/lab/lab.js
 else
 cat <<heredoc
