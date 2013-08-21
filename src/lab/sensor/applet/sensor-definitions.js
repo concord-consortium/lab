fc25bd8a26a8a135dcd665ad80b10695fc5277e0
Richard Klancer <rpk@pobox.com>
2013-08-21 12:32:00 -0400

    Add full list of non-pseudo sensor types
    IS [#54785632]

--- src/lab/sensor/applet/sensor-definitions.js
+++ src/lab/sensor/applet/sensor-definitions.js
@@ -16,12 +16,120 @@ define(function() {
       tareable: true
     },
 
+    goLinkTemperature: {
+      appletClass: 'goio',
+      measurementType: 'temperature',
+      sensorName: "GoIO Temperature Sensor",
+      deviceName: "GoIO",
+      samplesPerSecond: 10,
+      tareable: false
+    },
+
+    goLinkLight: {
+      appletClass: 'goio',
+      measurementType: 'lightIntensity',
+      sensorName: "GoIO Light Sensor",
+      deviceName: "GoIO",
+      samplesPerSecond: 10,
+      tareable: false
+    },
+
+    goLinkForce: {
+      appletClass: 'goio',
+      measurementType: 'force',
+      sensorName: "GoIO Force Sensor",
+      deviceName: "GoIO",
+      samplesPerSecond: 20,
+      tareable: true
+    },
+
+    goLinkPH: {
+      appletClass: 'goio',
+      measurementType: 'pH',
+      sensorName: "GoIO pH Sensor",
+      deviceName: "GoIO",
+      samplesPerSecond: 10,
+      tareable: false
+    },
+
+    goLinkCO2: {
+      appletClass: 'goio',
+      sensorName: "GoIO CO₂ sensor",
+      deviceName: "GoIO",
+      measurementType: 'ppm',
+      samplesPerSecond: 1,
+      tareable: false
+    },
+
+    goLinkO2: {
+      appletClass: 'goio',
+      sensorName: "GoIO O₂ sensor",
+      deviceName: "GoIO",
+      measurementType: 'ppm',
+      samplesPerSecond: 1,
+      tareable: false
+    },
+
+    labQuestMotion: {
+      appletClass: 'labquest',
+      measurementType: 'distance',
+      sensorName: "LabQuest Motion Sensor",
+      deviceName: "LabQuest",
+      samplesPerSecond: 20,
+      tareable: true
+    },
+
+    labQuestTemperature: {
+      appletClass: 'labquest',
+      measurementType: 'temperature',
+      sensorName: "LabQuest Temperature Sensor",
+      deviceName: "LabQuest",
+      samplesPerSecond: 10,
+      tareable: false
+    },
+
     labQuestLight: {
       appletClass: 'labquest',
       measurementType: 'lightIntensity',
       sensorName: "LabQuest Light Sensor",
       deviceName: "LabQuest",
       samplesPerSecond: 10,
+      tareable: false
+    },
+
+    labQuestForce: {
+      appletClass: 'labquest',
+      measurementType: 'force',
+      sensorName: "LabQuest Force Sensor",
+      deviceName: "LabQuest",
+      samplesPerSecond: 20,
+      tareable: true
+    },
+
+    labQuestPH: {
+      appletClass: 'labquest',
+      measurementType: 'pH',
+      sensorName: "labQuest pH Sensor",
+      deviceName: "LabQuest",
+      samplesPerSecond: 10,
+      tareable: false
+    },
+
+    labQuestCO2: {
+      appletClass: 'labquest',
+      sensorName: "LabQuest CO₂ sensor",
+      deviceName: "LabQuest",
+      measurementType: 'ppm',
+      samplesPerSecond: 1,
+      tareable: false
+    },
+
+    labQuestO2: {
+      appletClass: 'labquest',
+      sensorName: "LabQuest O₂ sensor",
+      deviceName: "LabQuest",
+      measurementType: 'ppm',
+      samplesPerSecond: 1,
       tareable: false
     }
   };
--- src/lab/sensor/units-definition.js
+++ src/lab/sensor/units-definition.js
@@ -13,10 +13,31 @@ define(function() {
         pluralName: "meters",
         symbol: "m"
       },
+      temperature: {
+        name: "degree Celsius",
+        pluaralName: "degrees Celsius",
+        symbol: "°C"
+      },
       lightIntensity: {
         name: "lux",
         pluralName: "lux",
         symbol: "lux"
+      },
+      force: {
+        name: "Newton",
+        pluralName: "Newtons",
+        symbol: "N"
+      },
+      // Yes, for some sensors, we willfully conflate type of quantity with its measurement units
+      pH: {
+        name: "pH Unit",
+        pluralName: "pH Units",
+        symbol: "pH"
+      },
+      ppm: {
+        name: "part per million",
+        pluralName: "parts per million",
+        symbol: "ppm"
       }
     }
   };
