2affc0cc153fa9e609e7d4bd0bb58d5dfc7a2ead
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-09-13 13:18:13 -0400

    button delegates more to interactiveComponent
    A buttonController instance now delegates modelLoaded
    events and click action/handler teardown-setup to
    interactiveComponent base class.

--- src/lab/common/controllers/button-controller.js
+++ src/lab/common/controllers/button-controller.js
@@ -2,29 +2,25 @@
 
 define(function () {
   var inherit              = require('common/inherit'),
-      InteractiveComponent = require('common/controllers/interactive-component');
+      InteractiveComponent = require('common/controllers/interactive-component'),
+
+      buttonControllerCount = 0;
 
   function ButtonController(component, scriptingAPI, interactivesController) {
-    this.scriptingAPI = scriptingAPI;
+    this._actionClickFunction = function () { };
+    this._nameSpace = "button" + (++buttonControllerCount);
     // Call super constructor.
     InteractiveComponent.call(this, "button", component, scriptingAPI, interactivesController);
     this.$element.addClass("interactive-button");
     this.button = $('<button>')
         .html(component.text)
-        .on("click", scriptingAPI.makeFunctionInScriptContext(component.action))
         .appendTo(this.$element);
   }
 
   inherit(ButtonController, InteractiveComponent);
 
-  ButtonController.prototype.setAction = function (newAction) {
-    this.component.action = newAction;
-    if (typeof this.component.action !== "function") {
-      // Create function from the string or array of strings.
-      this.button.on("click", this.scriptingAPI.makeFunctionInScriptContext(this.component.action));
-    } else {
-      this.button.on("click", this.component.action);
-    }
+  ButtonController.prototype.modelLoadedCallback = function (model, scriptingAPI) {
+    ButtonController.superClass._modelLoadedCallback.call(this, model, scriptingAPI);
   };
 
   return ButtonController;
