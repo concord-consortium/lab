78799c34afe98da4efc1776032145354e099d95a
Piotr Janik <janikpiotrek@gmail.com>
2013-09-20 11:50:45 +0200

    Implement simple tools for functions instrumentation
    Now you can require "common/performance" in any place in Lab
    and then call:
    
    performance.enterScope("scopeName");
    (...)
    performance.leaveScope("scopeName");
    
    Benchmarks can use these scopes and get the average time spent in scope
    or average frequency using:
    
    performance.getAvgTime("scopeName");
    performance.getAvgFreq("scopeName");  // in Hz
    
    Note that data will be collected *ONLY* when you explicitly enable it
    by calling:
    
    .collectData(true);
    
    So benchmark should do it at the beginning and when its tests end,
    it should call:
    
    .collectData(false)
    
    to ensure that there is no additional overhead when users run
    simulations without benchmarking.

--- src/lab/common/performance.js
+++ src/lab/common/performance.js
@@ -1,7 +1,9 @@
 /*global define, performance, $ */
 
 define(function () {
-  var nowFunc;
+  var nowFunc,
+      scope,
+      enabled = false;
 
   if (typeof performance !== 'undefined' && typeof performance.now !== 'undefined') {
     nowFunc = $.proxy(performance.now, performance);
@@ -14,7 +16,53 @@ define(function () {
      * window.performance.now or Date.now when performance.now is not available.
      * @type {Function}
      */
-    now: nowFunc
+    now: nowFunc,
+
+    collectData: function (v) {
+      enabled = v;
+      // Reset data each time when data collection is being started.
+      if (enabled) scope = {};
+    },
+
+    enterScope: function (name) {
+      if (enabled) {
+        var s = scope[name];
+        if (s === undefined) {
+          s = scope[name] = {
+            lastTime: 0,
+            timeSum: 0,
+            count: 0
+          };
+        }
+        s.lastTime = nowFunc();
+      }
+    },
+
+    leaveScope: function (name) {
+      if (enabled) {
+        var s = scope[name];
+        if (s !== undefined) {
+          s.timeSum += nowFunc() - s.lastTime;
+          s.count += 1;
+        }
+      }
+    },
+
+    getAvgTime: function (name) {
+      var s = scope[name];
+      if (s !== undefined) {
+        return s.timeSum / s.count;
+      }
+      return 0;
+    },
+
+    getAvgFreq: function (name) { // in Hz
+      var s = scope[name];
+      if (s !== undefined) {
+        return s.count * 1000 / s.timeSum;
+      }
+      return 0;
+    }
   };
 
 });
