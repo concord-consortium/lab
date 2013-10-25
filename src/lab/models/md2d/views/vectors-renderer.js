c8df3b7e3f333ffca63eaaf14baadb669b202ca2
Piotr Janik <janikpiotrek@gmail.com>
2013-10-25 19:04:05 +0200

    Implement velocity vectors using Pixi.Sprite
    [#58090222]
    
    They are faster. Of course code will need to be cleaned up when we are
    100% sure that it's a better approach. Now it's a useful reference.

--- src/lab/models/md2d/views/vectors-renderer.js
+++ src/lab/models/md2d/views/vectors-renderer.js
@@ -5,7 +5,8 @@ define(function(require) {
   var PIXI         = require('pixi'),
       color2number = require('common/views/color').color2number,
 
-      RENDERING_OPTIONS = ["showVelocityVectors", "velocityVectors"];
+      RENDERING_OPTIONS = ["showVelocityVectors", "velocityVectors"],
+      USE_SPRITES = true;
 
   return function VectorsRenderer(modelView, model, pixiContainer) {
     // Public API object to be returned.
@@ -16,6 +17,9 @@ define(function(require) {
 
         modelAtoms,
         velocityVectorScale,
+
+        container,
+        viewVelocityVectors,
 
         renderMode = {},
 
@@ -36,9 +40,33 @@ define(function(require) {
       });
     }
 
+    function getVelocityVectorTexture() {
+      var canv = document.createElement("canvas"),
+          ctx = canv.getContext("2d");
+
+      canv.width = 1;
+      canv.height = 1;
+      ctx.fillStyle = renderMode.velocityVectors.color;
+      ctx.fillRect(0, 0, 1, 1);
+
+      return new PIXI.Texture.fromCanvas(canv);
+    }
+
     function renderVelocityVector(atom) {
       graphics.moveTo(m2px(atom.x), m2pxInv(atom.y));
       graphics.lineTo(m2px(atom.x + atom.vx * velocityVectorScale), m2pxInv(atom.y + atom.vy * velocityVectorScale));
+    }
+
+    function renderVelocityVectorSprite(i) {
+      var vec = viewVelocityVectors[i],
+          atom = modelAtoms[i],
+          vx = atom.vx,
+          vy = atom.vy;
+
+      vec.position.x = m2px(atom.x);
+      vec.position.y = m2pxInv(atom.y);
+      vec.scale.y = m2px(Math.sqrt(vx * vx + vy * vy) * velocityVectorScale);
+      vec.rotation = Math.PI + Math.atan2(vx, vy);
     }
 
     function clear() {
@@ -58,6 +86,27 @@ define(function(require) {
         m2px = modelView.model2canvas;
         m2pxInv = modelView.model2canvasInv;
 
+        if (USE_SPRITES) {
+          var i, len, vec, tex;
+
+          viewVelocityVectors = [];
+
+          if (container) {
+            pixiContainer.removeChild(container);
+          }
+          container = new PIXI.DisplayObjectContainer();
+          pixiContainer.addChild(container);
+
+          tex = getVelocityVectorTexture();
+          for (i = 0, len = modelAtoms.length; i < len; ++i) {
+            vec = new PIXI.Sprite(tex);
+            vec.i = i;
+            vec.scale.x = m2px(renderMode.velocityVectors.width);
+            viewVelocityVectors.push(vec);
+            container.addChild(vec);
+          }
+        }
+
         api.update();
       },
 
@@ -70,11 +119,18 @@ define(function(require) {
       update: function () {
         if (!renderMode.showVelocityVectors) return;
         var i, len;
-        graphics.clear();
-        graphics.lineStyle(m2px(renderMode.velocityVectors.width * 1.2),
-                           color2number(renderMode.velocityVectors.color));
-        for (i = 0, len = modelAtoms.length; i < len; ++i) {
-          renderVelocityVector(modelAtoms[i]);
+
+        if (USE_SPRITES) {
+          for (i = 0, len = viewVelocityVectors.length; i < len; ++i) {
+            renderVelocityVectorSprite(i);
+          }
+        } else {
+          graphics.clear();
+          graphics.lineStyle(m2px(renderMode.velocityVectors.width),
+                             color2number(renderMode.velocityVectors.color));
+          for (i = 0, len = modelAtoms.length; i < len; ++i) {
+            renderVelocityVector(modelAtoms[i]);
+          }
         }
       }
     };
