506e0343c7d6ecea4201c95580052bd4fb91b992
Piotr Janik <janikpiotrek@gmail.com>
2014-02-04 21:16:59 +0100

    Update external-model experiment to use new iframe-phone
    [#65115678]

--- src/experiments/external-model/external-model.js
+++ src/experiments/external-model/external-model.js
@@ -1,9 +1,13 @@
+/* global iframePhone */
 var direction = "right",
     rightTarget = "250px",
     leftTarget = "50px";
-function moveBox(){
+
+var iframeEndpoint = iframePhone.getIFrameEndpoint();
+
+function moveBox() {
   var target;
-  if(direction == "right") {
+  if(direction === "right") {
     direction = "left";
     target = rightTarget;
   } else {
@@ -12,18 +16,20 @@ function moveBox(){
   }
   $(".block").animate({"left": target}, "slow", "swing",moveBox);
 }
-function start(){
+
+function start() {
   moveBox();
-  parentMessageController.post({type:'play.iframe-model'});
+  iframeEndpoint.post({type:'play.iframe-model'});
 }
 
-function stop(){
+function stop() {
   $(".block").stop();
-  parentMessageController.post({type:'stop.iframe-model'});
+  iframeEndpoint.post({type:'stop.iframe-model'});
 }
-parentMessageController.initialize();
-parentMessageController.addListener('play', start);
-parentMessageController.addListener('stop', stop);
+
+iframeEndpoint.initialize();
+iframeEndpoint.addListener('play', start);
+iframeEndpoint.addListener('stop', stop);
 // we are also going to get a listenForDispatchEvent message to add a listener for:
 //   'play.iframe-model', 'stop.iframe-model'
 // for now we aren't handling addDispatchListener,
