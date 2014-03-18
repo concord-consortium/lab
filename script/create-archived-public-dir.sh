1ad76da32aef33516b425a0d5ff53d9d743a9858
Piotr Janik <janikpiotrek@gmail.com>
2014-03-18 22:38:15 +0100

    Update script for creating archived public dir
    [#67398842]

--- script/create-archived-public-dir.sh
+++ script/create-archived-public-dir.sh
@@ -2,107 +2,24 @@
 #
 # script/create-archived-public-dir.sh [name-of-versioned-dir]
 #
-if ps aux | grep -v grep | grep 'ruby bin/guard' > /dev/null
-then
-cat <<heredoc
 
-*** shut down bin/guard first before generating a versioned public directory
-
-heredoc
-exit
-fi
-
-if git diff --exit-code --quiet && git diff --cached --exit-code --quiet
-then
-
-if [ ! -d public/.git ]
-then
-cat <<heredoc
-
-*** creating separate branch named 'public to track generated content
-
-heredoc
-cd public
-git init .
-git checkout -b public
-git checkout -b static
-git checkout public
-cp ../script/dot-gitignore-for-public .gitignore
-git add --all .
-git commit -am "generated from commit: `git --git-dir ../.git log -1 --format="%H%n%n%an <%ae>%n%cd%n%n    %s%n%n    %b"`"
-cd ..
-else
-cat <<heredoc
-
-*** updating separate branch named 'public to track generated content
-
-heredoc
-cd public
-if git checkout --quiet public
-then
-echo "checked out existing public branch"
-else
-git checkout -b public
-fi
-git add --all .
-git commit -am "generated from commit: `git --git-dir ../.git log -1 --format="%H%n%n%an <%ae>%n%cd%n%n    %s%n%n    %b"`"
-
-if git checkout --quiet static
-then
-echo "checked out existing static branch"
-else
-git checkout -b static
-fi
-git merge public
-cd ..
-LAB_DISABLE_MODEL_LIST=true  bin/haml -r ./script/setup.rb src/interactives.html.haml public/interactives.html
-LAB_DISABLE_MODEL_LIST=true  bin/haml -r ./script/setup.rb src/embeddable.html.haml public/embeddable.html
-rm -f src/lab/lab.config.js; make public/lab/lab.js STATIC=true
-make public/lab/lab.min.js
-cd public
-git commit -am "commit html files modified for static distribution into static branch"
-git checkout public
-cd ..
-fi
-
-# if no arg then use short SHA from commit as version name
-if [ -z "$1" ]
-  then
-    version=`git log -1 --format=%h`
-  else
-    version=$1
-fi
-
+version=$1
 archivename="$version.tar.gz"
 
-mkdir -p version
-cat <<heredoc
+echo "- create version/$version/public/ directory"
 
-***  copying public branch in ./public into: ./version/$version/public
+mkdir -p version/$version/public/
 
-heredoc
+echo "- copy public to version/$version/public/"
 
-cd public
-git checkout public
-git checkout-index -f -a --prefix=../version/$version/public/
+# Exclude:
+# .git - public folder used to be a git repo (previous version of this script was creating it),
+# version - symbolic links to other versions,
+# jnlp - following convention of the previous script, we also don't want to include that in archive.
+rsync -a --exclude='.git/' --exclude='version/' --exclude='jnlp/' public version/$version/
 
-cat <<heredoc
+echo "- generate $archivename archive"
 
-***  archiving static branch in ./public into: ./version/$archivename
-
-heredoc
-
-git checkout static
-git archive HEAD | gzip > ../version/$archivename
-git checkout public
-
-else
-cat <<heredoc
-
-*** uncommitted changes in your working directory
-
-    please commit or stash changes in your working dir before generating a versioned public directory
-
-heredoc
-git status
-fi
+mv version/$version/public/ version/$version/$version/
+tar -zcf version/$archivename --directory=version/$version/ $version
+mv version/$version/$version/ version/$version/public/
