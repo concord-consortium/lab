7856cac9edfc3151a01c110cdc4bcbfb2b4d72be
Scott Cytacki <scytacki@concord.org>
2014-03-25 07:14:10 -0400

    Deploy the production branch to the top level
    [#67763658]

--- script/s3-deploy.sh
+++ script/s3-deploy.sh
@@ -7,16 +7,19 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	exit 0
 fi
 
-# the 2> is to prevent error messages when no match is found
-CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
-if [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ]; then
-	mkdir -p "_site/version"
-	DEPLOY_DIR="version/$TRAVIS_BRANCH"
+if [ "$TRAVIS_BRANCH" = "production" ]; then
+  mv public _site
 else
-	mkdir -p "_site/branch"
-	DEPLOY_DIR="branch/$TRAVIS_BRANCH"
+  # the 2> is to prevent error messages when no match is found
+  CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
+  if [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ]; then
+    mkdir -p "_site/version"
+    DEPLOY_DIR="version/$TRAVIS_BRANCH"
+  else
+    mkdir -p "_site/branch"
+    DEPLOY_DIR="branch/$TRAVIS_BRANCH"
+  fi
+  mv public "_site/$DEPLOY_DIR"
+  export DEPLOY_DIR
 fi
-
-mv public "_site/$DEPLOY_DIR"
-export DEPLOY_DIR
 disable_parallel_processing=true bundle exec s3_website push --site _site --headless --config_dir config
