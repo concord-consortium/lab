01d535798734160b6ba5f5ba91088955c6d436a5
Scott Cytacki <scytacki@concord.org>
2014-03-26 23:38:02 -0400

    automatically create archives of tags on travis
    [#67763658]

--- script/s3-deploy.sh
+++ script/s3-deploy.sh
@@ -13,13 +13,21 @@ else
   # the 2> is to prevent error messages when no match is found
   CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
   if [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ]; then
-    mkdir -p "_site/version"
-    DEPLOY_DIR="version/$TRAVIS_BRANCH"
+    # this is a tag build
+    mkdir -p _site/version
+    DEPLOY_DIR=version/$TRAVIS_BRANCH
+    PREFIXED_NAME=lab-$TRAVIS_BRANCH
+    DEPLOY_ARCHIVE=version/$PREFIXED_NAME.tar.gz
+    mv public _site/version/$PREFIXED_NAME
+    tar -czf _site/$DEPLOY_ARCHIVE --directory=_site/version/ $PREFIXED_NAME
+    mv _site/version/$PREFIXED_NAME _site/$DEPLOY_DIR
   else
-    mkdir -p "_site/branch"
-    DEPLOY_DIR="branch/$TRAVIS_BRANCH"
+    mkdir -p _site/branch
+    DEPLOY_DIR=branch/$TRAVIS_BRANCH
+    DEPLOY_ARCHIVE=_fake_archive_that_should_never_be_on_the_server_
+    mv public _site/$DEPLOY_DIR
   fi
-  mv public "_site/$DEPLOY_DIR"
   export DEPLOY_DIR
+  export DEPLOY_ARCHIVE
 fi
 disable_parallel_processing=true bundle exec s3_website push --site _site --headless --config_dir config
