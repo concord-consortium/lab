5d486df4c7c7b8a403c2cd11df6a5c68c8ca17a0
Aaron Unger <aaron.unger@gmail.com>
2014-04-08 14:39:08 -0700

    SensorServer: Always use the server-supplied units
    This fixes a problem where multiple sensors have the same type, but are
    reporting different units and the wrong unit will be shown in the
    interface.

--- src/lab/models/sensor-server/units-definition.js
+++ src/lab/models/sensor-server/units-definition.js
@@ -5,41 +5,6 @@ define(function() {
         name: "second",
         pluralName: "seconds",
         symbol: "s"
-      },
-      distance: {
-        name: "meter",
-        pluralName: "meters",
-        symbol: "m"
-      },
-      temperature: {
-        name: "degree Celsius",
-        pluaralName: "degrees Celsius",
-        symbol: "°C"
-      },
-      light: {
-        name: "lux",
-        pluralName: "lux",
-        symbol: "lux"
-      },
-      force: {
-        name: "Newton",
-        pluralName: "Newtons",
-        symbol: "N"
-      },
-      ph: {
-        name: "pH Unit",
-        pluralName: "pH Units",
-        symbol: "pH"
-      },
-      co2: {
-        name: "part per million",
-        pluralName: "parts per million",
-        symbol: "ppm"
-      },
-      o2: {
-        name: "part per million",
-        pluralName: "parts per million",
-        symbol: "ppm"
       }
     }
   };
