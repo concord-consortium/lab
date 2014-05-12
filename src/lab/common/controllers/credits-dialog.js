064dc8572ab82346c742325e0d9e3832bc88c1cc
Piotr Janik <janikpiotrek@gmail.com>
2014-05-12 12:51:20 +0200

    Support translation of credits dialog when sharing == false
    [#69671536]
    
    + add Polish translation.

--- src/lab/common/controllers/credits-dialog.js
+++ src/lab/common/controllers/credits-dialog.js
@@ -29,32 +29,39 @@ define(function (require) {
         origin         = document.location.href.match(/(.*?\/\/.*?)\//)[1],
         embeddablePath = document.location.pathname,
         i18n           = this._i18n,
+        concordUrl     = 'http://concord.org',
+        nextGenUrl     = 'http://mw.concord.org/nextgen/',
+        interactiveCreditsUrl,
         utmString;
 
     this.set("title", this._i18n.t("credits_dialog.title", {interactive_title: interactive.title}));
 
-    var view = {
-      credits_text: i18n.t("credits_dialog.credits_text", {
-        CC_link: "<a class='opens-in-new-window' href='http://concord.org' target='_blank'>Concord Consortium</a>",
-        Next_Gen_MW_link: "<a class='opens-in-new-window' href='http://mw.concord.org/nextgen/' target='_blank'>Next-Generation Molecular Workbench</a>",
-        Google_link: "<a class='opens-in-new-window' href='http://www.google.org/' target='_blank'>Google.org</a>"
-      }),
-      copyright: getCopyright(i18n)
-    };
-
     if (labConfig.homeForSharing) {
-      view.interactiveCreditsUrl = labConfig.homeForSharing + labConfig.homeEmbeddablePath + hash;
+      interactiveCreditsUrl = labConfig.homeForSharing + labConfig.homeEmbeddablePath + hash;
     } else {
-      view.interactiveCreditsUrl = origin + embeddablePath + hash;
+      interactiveCreditsUrl = origin + embeddablePath + hash;
     }
 
     if (labConfig.utmCampaign) {
       utmString = "utm_source=" + encodeURIComponent(interactive.title.replace(/ /gi,"+")) +
         "&utm_medium=embedded_interactive&utm_campaign=" + labConfig.utmCampaign;
-      view.concordUrl += "?" + utmString;
-      view.nextGenUrl += "?" + utmString;
-      view.interactiveCreditsUrl += "?" + encodeURI("utm_source=embed_link&utm_medium=embedded_interactive&utm_campaign=" + labConfig.utmCampaign);
+      concordUrl += "?" + utmString;
+      nextGenUrl += "?" + utmString;
+      interactiveCreditsUrl += "?" + encodeURI("utm_source=embed_link&utm_medium=embedded_interactive&utm_campaign=" + labConfig.utmCampaign);
     }
+
+    var view = {
+      credits_text: i18n.t("credits_dialog.credits_text", {
+        CC_link: "<a class='opens-in-new-window' href='" + concordUrl + "' target='_blank'>Concord Consortium</a>",
+        Next_Gen_MW_link: "<a class='opens-in-new-window' href='" + nextGenUrl + " ' target='_blank'>Next-Generation Molecular Workbench</a>",
+        Google_link: "<a class='opens-in-new-window' href='http://www.google.org/' target='_blank'>Google.org</a>"
+      }),
+      find_shareable: i18n.t("credits_dialog.find_shareable", {
+        shareable_ver_link: '<a href="' + interactiveCreditsUrl + '" class="opens-in-new-window" target="_blank">' + i18n.t('credits_dialog.shareable_ver') + '</a>',
+        concord_org_link: '<a href="' + concordUrl + '" class="opens-in-new-window" target="_blank">concord.org</a>'
+      }),
+      copyright: getCopyright(i18n)
+    };
 
     if (!labConfig.sharing) {
       view.showShareable = true;
--- src/lab/common/controllers/credits-dialog.tpl
+++ src/lab/common/controllers/credits-dialog.tpl
@@ -4,9 +4,7 @@
   </p>
   {{#showShareable}}
   <p>
-    Find a <a href="{{interactiveCreditsUrl}}" class="opens-in-new-window" target="_blank">shareable version</a>
-    of this interactive along with dozens of other open-source interactives for science, math and engineering at
-    <a href="{{concordUrl}}" class="opens-in-new-window" target="_blank">concord.org</a>.
+    {{{find_shareable}}}
   </p>
   {{/showShareable}}
   {{{copyright}}}
