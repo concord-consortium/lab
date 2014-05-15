83102569e8b74525f16e9b17443e950fe0bb8203
Piotr Janik <janikpiotrek@gmail.com>
2014-05-15 19:21:24 +0200

    Setup grapher language when i18n is initialized
    [#71319240]

--- src/lab/common/i18n.js
+++ src/lab/common/i18n.js
@@ -2,6 +2,7 @@
 
 define(function (require) {
   var i18next       = require('i18next');
+  var LabGrapher    = require('lab-grapher');
   var translations  = JSON.parse(require('text!locales/translations.json'));
 
   return function i18n(language) {
@@ -11,6 +12,8 @@ define(function (require) {
       fallbackLng: 'en-US',
       useCookie: false
     });
+    // Grapher has its own i18n support implemented, just set language.
+    LabGrapher.i18n.lang = language;
     return i18next;
   };
 });
