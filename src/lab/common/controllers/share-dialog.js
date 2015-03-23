ae034966a08908aabc89ae2a70f0893409deee3a
ankg <aniketguptaknp@gmail.com>
2015-03-24 01:12:27 +0530

    Made share button loading faster, also passes all tests

--- src/lab/common/controllers/share-dialog.js
+++ src/lab/common/controllers/share-dialog.js
@@ -113,6 +113,10 @@ define(function (require) {
   *  Embeds tweet button
   */
   window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(typeof fjs === 'undefined') return;if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));
-
+  /**
+  *  Embeds the Google+ share button
+  */
+  (function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0];if(typeof s === 'undefined')return; s.parentNode.insertBefore(po, s); })();
+  
   return ShareDialog;
 });
