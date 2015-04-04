2ce78e4d40142262ba31c99eda2772dd16305a31
ankg <aniketguptaknp@gmail.com>
2015-04-04 23:19:43 +0530

    Changes tabs to 4 spaces and follows whitespace conventions

--- src/lab/common/controllers/spinner-controller.js
+++ src/lab/common/controllers/spinner-controller.js
@@ -11,123 +11,123 @@
 */
 define(function(){
 
-	var metadata   = require('common/controllers/interactive-metadata'),
-		validator  = require('common/validator'), 
-		disablable = require('common/controllers/disablable');
+  var metadata   = require('common/controllers/interactive-metadata'),
+      validator  = require('common/validator'), 
+      disablable = require('common/controllers/disablable');
 
-	return function SpinnerController(componenet, interactivesController){
-		var min, max, id, type, title, units,
-			stepSize, initialValue, displayValue,
-			propertyName, numberFormat,
-			$container,
-			$title,
-			$spinnerBox,
-			$spinnnerValue,
-			$spinnerButtons,
-			model,
-			scriptingAPI,
-			component;
-
-
-		// Binding the appropriate events to elements
-		function bindTargets(){
-
-		}
-
-		// Initializing the values and structure of the component
-		function initialize(){
-
-			scriptingAPI = interactivesController.getScriptingAPI();
-      		model = interactivesController.getModel();
-
-      		/**
-   			 * Validate the components' JSON  for completeness
-      		 */
-      		component = validator.validateCompleteness(metadata.spinner, component);
-			
-			// Initialize the spinner components
-			min = component.min;
-			max = component.max;
-			stepSize  = component.stepSize;
-			initialValue = component.initialValue;
-			displayValue = component.displayValue;
-			propertyName = component.property;
-			numberFormat = component.numberFormat;
-			title = component.title;
-			units = component.units;
-
-			if(stepSize === undefined) stepSize = 1;
-
-			// Initializing the view components
-			$container = $('<div class="spinner-container">');
-			$spinnerBox = $('<div class="html5-spinner">').attr(id, component.id);
-			$title = $('<p class="title">' + title + '</p>');
-			$title.appendTo($container);
-			$spinnerBox.appendTo($container);
-
-			$spinnerButtons = $spinnerBox.find("ui-spinner-button");
-
-			//Assign the tabIndex to every anchor or input component
-			$spinnerButtons.attr('tabindex', interactivesController.getNextTabindex());
-
-			// As each interactive component should have class component
-			$container.addClass('component');
-
-			// Space left for adding a tooltip and helpicon if needed later
+  return function SpinnerController(componenet, interactivesController){
+    var min, max, id, type, title, units,
+        stepSize, initialValue, displayValue,
+        propertyName, numberFormat,
+        $container,
+        $title,
+        $spinnerBox,
+        $spinnnerValue,
+        $spinnerButtons,
+        model,
+        scriptingAPI,
+        component;
 
 
-			//Applying the height and width to the spinner
-			$container.css({
-				width: component.width,
-				height: component.height
-			});
+    // Binding the appropriate events to elements
+    function bindTargets(){
 
-			if(component.width === "auto") $container.css({"min-width":"14em"});
+    }
 
-			// Initialize spinner with given values, the spinner comes in spinnerBox
-			$spinnerBox.spinner({
-				step: stepSize,
-				min : min,
-				max : max,
-				numberFormat : numberFormat
-			});
+    // Initializing the values and structure of the component
+    function initialize(){
 
-			bindTargets();
+      scriptingAPI = interactivesController.getScriptingAPI();
+      model = interactivesController.getModel();
 
-			$spinnerButtons.on('keydown.ui-spinner-button', function(event){
-				event.stopPropagation();
-			});
+      /**
+      * Validate the components' JSON  for completeness
+      */
+      component = validator.validateCompleteness(metadata.spinner, component);
+      
+      // Initialize the spinner components
+      min = component.min;
+      max = component.max;
+      stepSize  = component.stepSize;
+      initialValue = component.initialValue;
+      displayValue = component.displayValue;
+      propertyName = component.property;
+      numberFormat = component.numberFormat;
+      title = component.title;
+      units = component.units;
 
-			//Set the initial values to the spinner if it is defined
-			if(initialValue !== undefined && initialValue !== null) $spinnerBox.spinner('value',initialValue);
-		}
+      if(stepSize === undefined) stepSize = 1;
 
-		// Public API
-		controller = {
+      // Initializing the view components
+      $container = $('<div class="spinner-container">');
+      $spinnerBox = $('<div class="html5-spinner">').attr(id, component.id);
+      $title = $('<p class="title">' + title + '</p>');
+      $title.appendTo($container);
+      $spinnerBox.appendTo($container);
 
-			//This is triggered when model is loaded
-			modelLoadedCallback: function(){
+      $spinnerButtons = $spinnerBox.find("ui-spinner-button");
 
-			},
+      //Assign the tabIndex to every anchor or input component
+      $spinnerButtons.attr('tabindex', interactivesController.getNextTabindex());
 
-			//get the HTML container of the spinner 
-			getViewContainer: function(){
-				return $container;
-			},
+      // As each interactive component should have class component
+      $container.addClass('component');
 
-			//when the browser window resizes
-			resize: function(){
+      // Space left for adding a tooltip and helpicon if needed later
 
-			},
 
-			//Returns serialized component definition
-			serialize: function(){
+      //Applying the height and width to the spinner
+      $container.css({
+        width: component.width,
+        height: component.height
+      });
 
-			}
-		};
+      if(component.width === "auto") $container.css({"min-width":"14em"});
 
-		initialize();
+      // Initialize spinner with given values, the spinner comes in spinnerBox
+      $spinnerBox.spinner({
+        step: stepSize,
+        min : min,
+        max : max,
+        numberFormat : numberFormat
+      });
 
-		return controller;
-	};
+      bindTargets();
+
+      $spinnerButtons.on('keydown.ui-spinner-button', function(event){
+        event.stopPropagation();
+      });
+
+      //Set the initial values to the spinner if it is defined
+      if(initialValue !== undefined && initialValue !== null) $spinnerBox.spinner('value',initialValue);
+    }
+
+    // Public API
+    controller = {
+
+      //This is triggered when model is loaded
+      modelLoadedCallback: function(){
+
+      },
+
+      //get the HTML container of the spinner 
+      getViewContainer: function(){
+        return $container;
+      },
+
+      //when the browser window resizes
+      resize: function(){
+
+      },
+
+      //Returns serialized component definition
+      serialize: function(){
+
+      }
+    };
+
+    initialize();
+
+    return controller;
+  };
 });
