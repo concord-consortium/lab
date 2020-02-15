a0d742838abe150168479914595ac724fca84272
pjanik <janikpiotrek@gmail.com>
2020-02-15 23:19:52 +0100

    WIP: tests working without requirejs - updated babelrc, mocha opts and boilerplate
    [#171206851]

--- test/env.js
+++ test/env.js
@@ -1,30 +1,28 @@
-/*global screen: true, $: true, jQuery: true, Sizzle: true*/
-
 var fs = require('fs'),
     jsdom = require('jsdom'),
     html = fs.readFileSync('test/layout.html');
 
 process.env.TZ = "America/New_York";
 
-dom = new jsdom.JSDOM(html, {
+global.dom = new jsdom.JSDOM(html, {
   url: "https://lab.concord.org/",
   referrer: "https://lab.concord.org/",
 });
-window = dom.window;
-document = window.document;
-navigator = window.navigator;
-screen = window.screen;
+global.window = global.dom.window;
+global.document = window.document;
+global.navigator = window.navigator;
+global.screen = window.screen;
 
 // Sizzle is required for d3 to work well in a jsdom environment; we require the version from
 // node_modules instead of 'public' because d3 doesn't require Sizzle to operate in our target
 // browser environments.
-Sizzle = require('sizzle');
+global.Sizzle = require('sizzle');
 
 // Set up any vendored libraries that are normally included via script tag in the modules under test:
-require("../public/lab/vendor/d3/d3.min.js");
-$ = jQuery = window.jQuery = require("../public/lab/vendor/jquery/jquery.js");
+global.d3 = require("d3");
+global.$ = global.jQuery = require("jquery");
 // Setup libraries which depend on jQuery.
-require("../public/lab/vendor/jquery-ui/jquery-ui.js");
+require("jquery-ui");
 require("../public/lab/vendor/jquery-context-menu/jquery.contextMenu.js");
 require("../public/lab/vendor/jquery-selectBoxIt/jquery.selectBoxIt.min.js");
 
--- test/mocha-boilerplate.js
+++ test/mocha-boilerplate.js
@@ -1,11 +1,5 @@
-/*global requirejs: true, sinon: true */
-
-var helpers = require('./helpers');
-
 // Make these global and therefore predefined in Mocha tests:
-sinon     = require('sinon');
-requirejs = helpers.getRequireJS();
-
+global.sinon = require('sinon');
 // The following is required because the require('should') defines a getter-only property called
 // 'should' on Object.prototype. Since the global object inherits from Object, it becomes impossible
 // therefore to set the global variable 'should'. But we can fight fire with fire, and fix it by
